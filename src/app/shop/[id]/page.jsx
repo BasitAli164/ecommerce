35bf0420bdcc2de784663d57511f8f7938662919
Basit Ali <opoali911@gmail.com>
2025-06-13 22:20:44 +0500

    working in product detail page

--- src/app/shop/[id]/page.jsx
+++ src/app/shop/[id]/page.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import { shoeColors } from "@/lib/dammyData";
 export default async function ProductDetail({params}) {
     const {id}=await params;
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
-      <h1 className=' text-5xl '>Product Detail Page</h1>
-      <p>{id}</p>
+   <div className="w-full flex justify-center items-center">
+     <div className='max-w-[1400px] flex items-center justify-center gap-20 bg-gray-800 '>
+     <div className="">
+        {
+            shoeColors.map((item,index)=>(
+                <div key={index} className={`w-20 h-20 bg-[${item}] border `}></div>
+            ))
+        }
+     </div>
+     <div className="border-2 w-96 h-96 "></div>
+     <div className="border-2 w-96 h-96 "></div>
     </div>
+   </div>
   )
 }
