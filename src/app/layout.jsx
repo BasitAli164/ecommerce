2b391387f6c7de1f519914ae1602bd485130f73a
Basit Ali <opoali911@gmail.com>
2025-06-14 21:19:33 +0500

    change the layout codes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Header/>
         {children}
         <Footer/>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-import Header from "@/components/header/header";
 import HomePageComponent from "@/components/home/HomePageComponent";
 export default function Home() {
   return (
