0c70a8a515903acdbf35cab0d85ab4422c60a506
Basit Ali <opoali911@gmail.com>
2025-06-21 23:41:35 +0500

    now working with hover image

--- shoes/src/components/cart/HomePageCart.jsx
+++ shoes/src/components/cart/HomePageCart.jsx
@@ -1,6 +1,6 @@
 import HomePageCartBtn from "../button/HomePageCartBtn"
 
-function HomePageCart({btnText,img,link}) {
+function HomePageCart({btnText,img,link ,hoverImage}) {
   return (
    <div className="w-[250px] sm:w-[350px] h-[250px] sm:h-[350px] flex justify-center items-center rounded-full bg-amber-400" style={{backgroundImage:`url(${img})`, backgroundPosition:"center",backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
     <HomePageCartBtn btnText={btnText} link={link}/>   
