55fb62ea5da7a3bfbc2ef0db3223d198515f64e7
Basit Ali <opoali911@gmail.com>
2025-06-21 23:46:51 +0500

    almost complete the home page

--- shoes/src/components/cart/HomePageCart.jsx
+++ shoes/src/components/cart/HomePageCart.jsx
@@ -1,11 +1,19 @@
-import HomePageCartBtn from "../button/HomePageCartBtn"
+import HomePageCartBtn from "../button/HomePageCartBtn";
 
-function HomePageCart({btnText,img,link ,hoverImage}) {
+function HomePageCart({ btnText, img, link, hoverImage }) {
   return (
-   <div className="w-[250px] sm:w-[350px] h-[250px] sm:h-[350px] flex justify-center items-center rounded-full bg-amber-400" style={{backgroundImage:`url(${img})`, backgroundPosition:"center",backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
-    <HomePageCartBtn btnText={btnText} link={link}/>   
-   </div>
-  )
+    <div
+      className="w-[250px] sm:w-[350px] h-[250px] sm:h-[350px] flex justify-center items-center rounded-full bg-amber-400"
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <HomePageCartBtn btnText={btnText} link={link} />
+    </div>
+  );
 }
 
-export default HomePageCart
+export default HomePageCart;
