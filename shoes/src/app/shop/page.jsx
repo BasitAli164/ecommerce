9ad2b8123b577f92adbaca8e70d69593d61576a0
Basit Ali <opoali911@gmail.com>
2025-06-22 21:10:36 +0500

    now start create pages

---
+++ shoes/src/app/shop/page.jsx
@@ -0,0 +1,7 @@
+export default function Shop() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center '>
+      <h1 className='text-6xl font-bold'>Shop Page</h1>
+    </div>
+  )
+}
