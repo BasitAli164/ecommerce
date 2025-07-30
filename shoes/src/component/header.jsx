61ddb3991cffa69cb56acea01e8b45b127a4bca9
Basit Ali <opoali911@gmail.com>
2025-07-30 10:46:26 +0500

    working on the structure of the header component

--- shoes/src/component/header.jsx
+++ shoes/src/component/header.jsx
@@ -1,8 +1,28 @@
+import Link from "next/link"
 
 export default function Header() {
-
+const menulist=[
+  {id:1,menuName:"Home",link:"/"},
+  {id:2,menuName:"Product",link:"/product"},
+  {id:3,menuName:"Service",link:"/service"},
+  {id:4,menuName:"About",link:"/about"},
+  {id:5,menuName:"Contact",link:"/contact"},
+]
   return (
     <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            {
+                menulist.map(({id,menuName,link})=>(
+                    <ul key={id}>
+                        <Link href={link}>{menuName}</Link>
+                    </ul>
+                ))
+            }
+        </div>
+
       
     </div>
   )
