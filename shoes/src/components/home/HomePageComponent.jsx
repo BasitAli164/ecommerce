e81002b25ae39b45c58c41f0c0aeb688f925b4e0
Basit Ali <opoali911@gmail.com>
2025-07-31 18:50:55 +0500

    working on home page

--- shoes/src/components/home/HomePageComponent.jsx
+++ shoes/src/components/home/HomePageComponent.jsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import ButtonComp from "../button/button";
 
 export default function HomePageComponent() {
   return (
-    <div>
-      <div></div>
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-5">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <h1 className="text-2xl sm:text-3xl lg:text-6xl font-bold">Welcome to Our Shop</h1>
+        <h4 className="text-lg sm:text-xl lg:text-3xl font-bold">Discover our latest collection</h4>
+        <p className="text-lg sm:text-xl md:text-2xl lg:text-2xl text-center">Explore our wide range of high-quality products. We offer the best selection of goods for <Link href={'/men'} className="font-bold text-xl underline">Men</Link> and <Link href={'/women'} className="font-bold text-xl underline">Women</Link> . Whether you're looking for the latest trends or timeless classics, we have something for everyone. Our commitment to quality ensures that you get the best value for your money.</p>
+      </div>
       <ButtonComp bgColor={"#17a589"} btnText={"Shop Now"}/>
     </div>
   )
