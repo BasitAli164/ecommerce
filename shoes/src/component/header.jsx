2e11fd161328cb74ab2b1b8d7f49f7c7b2100aba
Basit Ali <opoali911@gmail.com>
2025-07-31 10:57:31 +0500

    working on the mobile menu logic

--- shoes/src/component/header.jsx
+++ shoes/src/component/header.jsx
@@ -14,32 +14,50 @@ const menulist = [
 
 export default function Header() {
   const [toggleMenu , setToggleMenu]=useState(false)
+  console.log("toggel: ",toggleMenu)
 
 
   return (
-    <div className="w-full flex items-center justify-between bg-[#17a589] px-5">
+    <div className="w-full flex items-center justify-evenly sm:justify-between bg-[#17a589] sm:px-5">
       <div>
         <Link href={"/"}>
           {" "}
           <Image
-            className="hover:cursor-pointer w-20 h-20"
+            className="hover:cursor-pointer w-10 h-10 sm:w-20 sm:h-20"
             src={logo}
             alt="Logo"
             
           />
         </Link>
       </div>
-      <div className="flex items-center justify-center gap-5">
+     <div className="flex justify-center items-center">
+       <div className="flex items-center justify-center gap-5">
         {menulist.map(({ id, menuName, link }) => (
           <ul key={id}>
-            <Link className="font-bold text-lg" href={link}>{menuName}</Link>
+            <Link className="font-bold text-lg hidden sm:block" href={link}>{menuName}</Link>
           </ul>
         ))}
       </div>
-      <div className="flex items-center justify-center gap-5">
-        <User size={25} color="white" />
-        <HelpCircleIcon size={25} color="white" />
-        <ShoppingCartIcon size={25} color="white" />
+      <div className="relative">
+        <MenuIcon className="sm:hidden cursor-pointer transition duration-700 delay-100 ease-in-out " onClick={()=>setToggleMenu(!toggleMenu)}/>
+          {
+            toggleMenu &&(
+              <div className="bg-red-400 w-40 h-20 absolute top-8 -left-18 ">
+                {menulist.map(({ id, menuName, link }) => (
+          <ul key={id} className="text-center">
+            <Link className="font-bold text-lg " href={link}>{menuName}</Link>
+          </ul>
+        ))}
+              </div>
+
+            )
+          }
+      </div>
+     </div>
+      <div className="flex items-center justify-center gap-2 sm:gap-5">
+        <User className="w-5 h-5 text-white sm:w-7 sm:h-7" />
+        <HelpCircleIcon className="w-5 h-5 text-white sm:w-7 sm:h-7" />
+        <ShoppingCartIcon className="w-5 h-5 text-white sm:w-7 sm:h-7" />
       </div>
     </div>
   );
