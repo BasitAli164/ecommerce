d1858161c6bf3969b49ce2fabd038f2dbe86b0fa
Basit Ali <opoali911@gmail.com>
2025-08-02 18:18:16 +0500

    almost ready

--- shoes/src/components/home/HomeFooter.jsx
+++ shoes/src/components/home/HomeFooter.jsx
@@ -13,12 +13,12 @@ const cartData=[
 export default function HomeFooter() {
   return (
     <section className="w-full   overflow-hidden bg-gray-300">
-        <div className="  p-5 flex flex-wrap justify-evenly items-center  gap-5">
+        <div className="  p-5 flex flex-wrap justify-evenly items-center  gap-2">
             {
                 data.map((item)=>(
-                    <div key={item.id} className="h-[600px] w-[500px] flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat rounded-4xl" style={{backgroundImage:`url(${item.img})`}}>
-                        <h1>{item.name}</h1>
-                        <p>{item.description}</p>
+                    <div key={item.id} className="h-[600px] w-[600px] flex flex-col justify-center items-center bg-cover bg-no-repeat transition duration-700 delay-100 ease-in-out transform hover:scale-[1.01] rounded-4xl hover:cursor-pointer" style={{backgroundImage:`url(${item.img})`}}>
+                        <h1 className="font-bold sm:text-2xl lg:text-4xl text-gray-300 ">{item.name}</h1>
+                        <p className="sm:text-lg lg:text-xl font-thin text-center">{item.description}</p>
 
                     </div>
                 ))
