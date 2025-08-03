707c6950568880484b72ae708d00f5a561222918
Basit Ali <opoali911@gmail.com>
2025-08-03 08:33:29 +0500

    working on cart component

--- shoes/src/components/cart/Cart.jsx
+++ shoes/src/components/cart/Cart.jsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 
 const dammyData=[
-    {id:1,title:"Shoe-1",price:1234,}
+    {id:1,title:"Shoe-1",price:120,image:"/HomeCartsImage/img1.webp"},
+    {id:1,title:"Shoe-2",price:134,image:  "/HomeCartsImage/img2.webp"},,
+    {id:1,title:"Shoe-3",price:224,image:  "/HomeCartsImage/img3.webp"},,
+    {id:1,title:"Shoe-4",price:400,image:  "/HomeCartsImage/img4.webp"},,
 ]
 
 export default function Cart() {
-  return <div>
+  return <>
+  {
+    dammyData.map((item)=>(
+        <div key={item.id}>
+            
+
+        </div>
+    ))
+  }
     
-  </div>;
+  </>;
 }
