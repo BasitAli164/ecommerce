e62ffe6fc208fbb0665f7c3efe557345a9896a02
Basit Ali <opoali911@gmail.com>
2025-08-03 23:45:03 +0500

    working on the cart component of the shoes

--- shoes/src/components/button/RadioButton.jsx
+++ shoes/src/components/button/RadioButton.jsx
@@ -8,7 +8,7 @@ export default function RadioButton() {
     <>
     {
         colors.map((item,index)=>(
-            <button key={index} className={`px-1 py-1 rounded-full bg-['${item}']`}>            
+            <button key={index} className={`px-2 py-2 mx-0.5 rounded-full `} style={{backgroundColor:item}}>            
     </button>
         ))
     }
--- shoes/src/components/cart/Cart.jsx
+++ shoes/src/components/cart/Cart.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import RadioButton from "../button/RadioButton";
 
 const dammyData=[
-    {id:1,title:"Shoe-1",price:120,image:"/HomeCartsImage/img1.webp"},
-    {id:1,title:"Shoe-2",price:134,image:  "/HomeCartsImage/img2.webp"},
-    {id:1,title:"Shoe-3",price:224,image:  "/HomeCartsImage/img3.webp"},
-    {id:1,title:"Shoe-4",price:400,image:  "/HomeCartsImage/img4.webp"},
+    {id:1,title:"Shoe-1",price:120,image:"/CartImage/img1.webp"},
+    {id:1,title:"Shoe-2",price:134,image:  "/CartImage/img1.webp"},
+    {id:1,title:"Shoe-3",price:224,image:  "/CartImage/img1.webp"},
+    {id:1,title:"Shoe-4",price:400,image:  "/CartImage/img1.webp"},
 ]
 
 export default function Cart() {
@@ -13,14 +13,14 @@ export default function Cart() {
   {
     dammyData.map((item)=>(
         <div key={item.id} className="w-[250px] h-[330px] p-3 rounded-3xl shadow bg-gray-200 text-black">
-            <div className="w-[200px] h-[200px] bg-amber-100 bg-center bg-cover bg-no-repeat " style={{backgroundImage:`url(${item.image})`}}>
+            <div className="w-[230px] h-[230px] bg-center bg-cover bg-no-repeat " style={{backgroundImage:`url(${item.image})`}}>
                 
             </div>
             <div className>
                 <p>{item.title}</p>
                 <div>
-                    <RadioButton/>
                     <p>${item.price}</p>
+                    <RadioButton/>
                 </div>
 
             </div>
