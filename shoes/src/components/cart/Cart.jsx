bd628b2523e12692a49f3a683f02ac1464a69f4d
Basit Ali <opoali911@gmail.com>
2025-08-03 08:28:21 +0500

    making cart compnent

--- shoes/src/components/cart/Cart.jsx
+++ shoes/src/components/cart/Cart.jsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
+const dammyData=[
+    {id:1,title:"Shoe-1",price:1234,}
+]
+
 export default function Cart() {
-  return <div></div>;
+  return <div>
+    
+  </div>;
 }
