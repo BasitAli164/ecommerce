bb44dbd2790cbe0014e2c46a9c682038b2c11565
Basit Ali <opoali911@gmail.com>
2025-08-05 22:47:45 +0500

    filter component

--- shoes/src/components/home/HomePageComponent.jsx
+++ shoes/src/components/home/HomePageComponent.jsx
@@ -112,9 +112,7 @@ export default function HomePageComponent() {
       </div>
       <div>
         <HomeFooter/>
-       <div className="flex gap-4 p-5">
-         <Cart/>
-       </div>
+       
       </div>
     </>
   );
