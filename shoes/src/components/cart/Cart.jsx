b2307b149c493cce72c3d03fcc21f67d29e7513c
Basit Ali <opoali911@gmail.com>
2025-08-06 11:53:11 +0500

    create a global file for dammy data

--- shoes/src/components/cart/Cart.jsx
+++ shoes/src/components/cart/Cart.jsx
@@ -1,30 +1,11 @@
-import Image from "next/image";
 import RadioButton from "../button/RadioButton";
 import { LucideShoppingCart } from "lucide-react";
-
-export const dammyData = [
-  {
-    id: 1,
-    title: "Shoe-1",
-    price: 120,
-    image: "/CartImage/img1.webp",
-    new: "NEW",
-  },
-  { id: 2, title: "Shoe-2", price: 134, image: "/CartImage/img1.webp" },
-  { id: 3, title: "Shoe-3", price: 224, image: "/CartImage/img1.webp" },
-  {
-    id: 4,
-    title: "Shoe-4",
-    price: 400,
-    image: "/CartImage/img1.webp",
-    new: "NEW",
-  },
-];
+import { dammyDataForCarts } from "@/lib/dammyData";
 
 export default function Cart() {
   return (
     <>
-      {dammyData.map((item) => (
+      {dammyDataForCarts.map((item) => (
         <div
           key={item.id}
           className="w-[320px] h-[420px]  flex flex-col  justify-center p-2 rounded-3xl shadow bg-gray-200 text-black cursor-pointer"
---
+++ shoes/src/components/filter/FilterBox.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+export default function FilterBox() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
