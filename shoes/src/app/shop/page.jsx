27d5233643196685a214abff64e9017803002f0b
Basit Ali <opoali911@gmail.com>
2025-08-06 11:43:36 +0500

    remove the border

--- shoes/src/app/shop/page.jsx
+++ shoes/src/app/shop/page.jsx
@@ -3,8 +3,8 @@ import Cart from "@/components/cart/Cart";
 
 export default function Shop() {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center border-4 border-red-800 py-10">
-      <div className="w-full max-w-7xl text-center border-4 border-green-800 p-6">
+    <div className="w-full min-h-screen flex flex-col items-center  py-10">
+      <div className="w-full max-w-7xl text-center p-6">
         <h1 className="text-6xl font-bold mb-8 mt-16">Shop Page</h1>
 
         {/* Filter Section */}
