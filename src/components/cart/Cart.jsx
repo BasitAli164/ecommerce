18dcfb8b6d817e636aa8b87dda6d9b37d13d6827
Basit Ali <opoali911@gmail.com>
2025-08-14 17:52:10 +0500

    update the cart and filter components

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -16,15 +16,10 @@ export default function Cart() {
             className="max-w-[300px] h-[300px] bg-center bg-cover bg-no-repeat "
             style={{ backgroundImage: `url(${item.image})` }}
           ></div>
-          <div>
+          <div className="flex justify-between items-center">
             {" "}
             <p className="text-xl font-semibold">{item.title}</p>
-            <div className="flex justify-between items-center">
-              <p>
-                <RadioButton />
-              </p>
-              <p className="text-lg">${item.price}</p>
-            </div>
+            <p className="text-lg">${item.price}</p>
           </div>
         </div>
       ))}
--- src/lib/dammyData.js
+++ src/lib/dammyData.js
@@ -3,7 +3,7 @@ export const shoeSize=[3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5, 9,9.5, 10]
 
 // Colors of Shoes
 
-export const shoeColors=['black',"beige","blue","brown","grey","red","pink"]
+export const shoeColors=['darkred','darkblue','darkgreen',"beige","black","blue","brown","grey","orange","red","pink"]
 
 
 // chcek list of Product Brands/Types
