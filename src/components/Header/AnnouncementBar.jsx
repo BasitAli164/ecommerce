35d45a4cb328b8dcb13cae8c68f89d7ae165bf55
Basit Ali <opoali911@gmail.com>
2025-08-15 15:37:24 +0500

    working in annoucment bar component

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 export default function AnnouncementBar() {
   return (
-    <div className='w-screen h-12'>
-      <div>
-        <p>
+    <div className='w-screen  bg-bgPrimary  '>
+      <div className='w-full p-4 flex justify-around  items-center  overflow-hidden gap-10'>
+        <p className='text-secondaryText text-sm  text-center hidden sm:block'>
           Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-          <Link href={""}>Shop Now</Link>
         </p>
+          <Link href={""} className='border-b-2 border-white border-solid font-semibold text-sm ml-2 text-secondaryText  '>Shop Now</Link>
 
-        <select name="language" id="language">
-          <option value="US-Eng">English(US)</option>
-          <option value="EN-Eng">English(EN)</option>
-          <option value="AU-Eng">English(AU)</option>
-          <option value="NZ-Eng">English(NZ)</option>
+        <select className='text-secondaryText border-none outline-none text-sm' name="language" id="language">
+          <option className='text-primaryText' value="US-Eng">English(US)</option>
+          <option className='text-primaryText' value="EN-Eng">English(EN)</option>
+          <option className='text-primaryText' value="AU-Eng">English(AU)</option>
+          <option className='text-primaryText' value="NZ-Eng">English(NZ)</option>
         </select>
       </div>
       
