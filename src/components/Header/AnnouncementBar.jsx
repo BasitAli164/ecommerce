4564c85c16ecd65b292f67fd8566d57f50bd72a4
Basit Ali <opoali911@gmail.com>
2025-08-15 15:43:24 +0500

    almost ready the annoucement bar

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -5,12 +5,12 @@ export default function AnnouncementBar() {
   return (
     <div className='w-screen  bg-bgPrimary  '>
       <div className='w-full p-4 flex justify-around  items-center  overflow-hidden gap-10'>
-        <p className='text-secondaryText text-sm  text-center hidden sm:block'>
+        <p className='text-secondaryText text-[10px] md:text-sm  text-center  '>
           Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          <Link href={""} className='border-b-2 border-white border-solid font-semibold text-[10px] md:text-sm ml-2 text-secondaryText  '>Shop Now</Link>
         </p>
-          <Link href={""} className='border-b-2 border-white border-solid font-semibold text-sm ml-2 text-secondaryText  '>Shop Now</Link>
 
-        <select className='text-secondaryText border-none outline-none text-sm' name="language" id="language">
+        <select className='text-secondaryText border-none outline-none text-[10px] md:text-sm' name="language" id="language">
           <option className='text-primaryText' value="US-Eng">English(US)</option>
           <option className='text-primaryText' value="EN-Eng">English(EN)</option>
           <option className='text-primaryText' value="AU-Eng">English(AU)</option>
