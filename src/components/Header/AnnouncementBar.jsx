7f027bf8c50151ebab153b0a5a9dca60e3b7a28c
Basit Ali <opoali911@gmail.com>
2025-08-15 15:20:00 +0500

    start again to work in ecommerce website

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function AnnouncementBar() {
   return (
-    <div>
+    <div className='w-screen h-12'>
+      <div>
+        <p>
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          <Link href={""}>Shop Now</Link>
+        </p>
+      </div>
       
     </div>
   )
