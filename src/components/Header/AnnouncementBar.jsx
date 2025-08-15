43fc6609f3103059bed5fb0181706e30c92d0478
Basit Ali <opoali911@gmail.com>
2025-08-15 15:21:50 +0500

    structure of the announcement bar component is ready

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -9,6 +9,13 @@ export default function AnnouncementBar() {
           Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
           <Link href={""}>Shop Now</Link>
         </p>
+
+        <select name="language" id="language">
+          <option value="US-Eng">English(US)</option>
+          <option value="EN-Eng">English(EN)</option>
+          <option value="AU-Eng">English(AU)</option>
+          <option value="NZ-Eng">English(NZ)</option>
+        </select>
       </div>
       
     </div>
