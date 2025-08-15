08c86b16e7e2dc80be790d985501dfefb465a5d0
Basit Ali <opoali911@gmail.com>
2025-08-15 15:49:40 +0500

    announcement bar component is complete

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -1,23 +1,38 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function AnnouncementBar() {
-  
   return (
-    <div className='w-screen  bg-bgPrimary  '>
-      <div className='w-full p-4 flex justify-around  items-center  overflow-hidden gap-10'>
-        <p className='text-secondaryText text-[10px] md:text-sm  text-center  '>
+    <div className="w-screen  bg-bgPrimary  ">
+      <div className="w-full p-4 flex justify-around  items-center  overflow-hidden gap-10">
+        <p className="text-secondaryText text-[10px] md:text-sm  text-center  ">
           Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-          <Link href={""} className='border-b-2 border-white border-solid font-semibold text-[10px] md:text-sm ml-2 text-secondaryText  '>Shop Now</Link>
+          <Link
+            href={""}
+            className="border-b-2 border-white border-solid font-semibold text-[10px] md:text-sm ml-2 text-secondaryText  "
+          >
+            Shop Now
+          </Link>
         </p>
 
-        <select className='text-secondaryText border-none outline-none text-[10px] md:text-sm' name="language" id="language">
-          <option className='text-primaryText' value="US-Eng">English(US)</option>
-          <option className='text-primaryText' value="EN-Eng">English(EN)</option>
-          <option className='text-primaryText' value="AU-Eng">English(AU)</option>
-          <option className='text-primaryText' value="NZ-Eng">English(NZ)</option>
+        <select
+          className="text-secondaryText border-none outline-none text-[10px] md:text-sm"
+          name="language"
+          id="language"
+        >
+          <option className="text-primaryText" value="US-Eng">
+            English(US)
+          </option>
+          <option className="text-primaryText" value="EN-Eng">
+            English(EN)
+          </option>
+          <option className="text-primaryText" value="AU-Eng">
+            English(AU)
+          </option>
+          <option className="text-primaryText" value="NZ-Eng">
+            English(NZ)
+          </option>
         </select>
       </div>
-      
     </div>
-  )
+  );
 }
