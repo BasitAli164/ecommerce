a53ead3f68c21519cb4abfb2c1beb25378e96b5b
Basit Ali <opoali911@gmail.com>
2025-08-16 16:05:36 +0500

    explore new logics

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Heart, SearchIcon, ShoppingCart } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+
+  const [toggle ,setToggle]=useState(false)
+  const pathname=usePathname()
+const menulist=[
+  {href:"/",label:"Home"},
+  {href:"/contact",label:"Contact"},
+  {href:"/about",label:"About"},
+  {href:"/signup",label:"SignUp"},
+
+]
+
+
+
   return (
-    <div className='w-screen'>
+    <nav className=''>
+      <div>
+        <h1>Exclusive</h1>
+      </div>
+      <div>
+        <ul>
+        {
+          menulist.map((item)=>(
+            <li key={item.href}>
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))
+        }
+        </ul>
+
+      </div>
+      <div>
+        <input type="text" name="" id="" />
+        <SearchIcon/>
+      </div>
+      <div>
+        {
+          pathname==='/signup'?<>
+          </>:<><Heart/>
+        <ShoppingCart/></>
+        }
+
+      </div>
+
+
       
-    </div>
+    </nav>
   )
 }
