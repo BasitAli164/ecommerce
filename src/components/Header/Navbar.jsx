cb2fdbc5ce7214a92626547d64dc0bd2a0190b32
Basit Ali <opoali911@gmail.com>
2025-08-16 20:31:57 +0500

    working in navbar component

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,9 +5,9 @@ import React, { useState } from "react";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
-  const[fieldText,setFieldText]=useState("")
+  const [fieldText, setFieldText] = useState("");
   const pathname = usePathname();
-      console.log(fieldText)
+  console.log(fieldText);
 
   const menulist = [
     { href: "/", label: "Home" },
@@ -16,45 +16,53 @@ export default function Navbar() {
     { href: "/signup", label: "SignUp" },
   ];
 
-  const filterProducts=()=>{
-    console.log(fieldText)
-   setFieldText("")
-   console.log(fieldText,"after")
-
-  }
+  const filterProducts = () => {
+    console.log(fieldText);
+    setFieldText("");
+    console.log(fieldText, "after");
+  };
 
   return (
-    <nav className="w-full h-12 flex justify-around items-center p-4 border-b">
-      <h1 className="text-lg font-bold">Exclusive</h1>
-
-      <ul className="hidden sm:flex gap-5">
-        {menulist.map((item) => (
-          <li key={item.href} className="">
-            <Link href={item.href} className={pathname===item.href?"border-b-2 border-gray-400":""}>{item.label}</Link>
-          </li>
-        ))}
-      </ul>
-
-     <div className="flex gap-8">
-       <div className="flex bg-[#F5F5F5] opacity-70 rounded-md p-1  ">
-        <input
-          className="outline-none border-none hidden sm:inline"
-          placeholder="What are you looking for?"
-          type="text"
-          name=""
-          id=""
-          value={fieldText}
-          onChange={(e)=>setFieldText(e.target.value)}
-        />
-        <SearchIcon onClick={filterProducts}/>
+    <nav className="w-full bg-amber-200 flex flex-col justify-around items-center p-2 border-b border-gray-400 border-solid">
+      <div className="w-full flex justify-between items-center">
+        <h1 className="text-lg font-bold">Exclusive</h1>
+        <div className="flex gap-2">
+          <Heart />
+          <ShoppingCart />
+          <User />
+          <Menu className="md:hidden" />
+        </div>
       </div>
-      <div className="flex gap-2">
-        <Heart />
-        <ShoppingCart />
-        <User />
-        <Menu className="sm:hidden" />
+
+      <div className="w-full flex justify-center items-center gap-28 mt-4">
+        <ul className="hidden md:flex gap-5">
+          {menulist.map((item) => (
+            <li key={item.href} className="text-lg">
+              <Link
+                href={item.href}
+                className={
+                  pathname === item.href ? "border-b-2 border-gray-400" : ""
+                }
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className="w-[600px] md:w-60 flex justify-between items-center  bg-[#F5F5F5] opacity-70 rounded-full p-1  ">
+          <input
+            className="outline-none w-full md:w-60 rounded-full   "
+            placeholder="What are you looking for?"
+            type="text"
+            name="searchfield"
+            id="searchfield"
+            value={fieldText}
+            onChange={(e) => setFieldText(e.target.value)}
+          />
+          <SearchIcon onClick={filterProducts} />
+        </div>
       </div>
-     </div>
     </nav>
   );
 }
