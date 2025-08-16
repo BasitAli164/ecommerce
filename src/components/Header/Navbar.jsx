4ed0596ad6edc00c94e405bb0f4034a345411d0b
Basit Ali <opoali911@gmail.com>
2025-08-16 19:45:21 +0500

    add functionality

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,7 +5,10 @@ import React, { useState } from "react";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
+  const[fieldText,setFieldText]=useState("")
   const pathname = usePathname();
+      console.log(fieldText)
+
   const menulist = [
     { href: "/", label: "Home" },
     { href: "/contact", label: "Contact" },
@@ -13,26 +16,37 @@ export default function Navbar() {
     { href: "/signup", label: "SignUp" },
   ];
 
+  const filterProducts=()=>{
+    console.log(fieldText)
+   setFieldText("")
+   console.log(fieldText,"after")
+
+  }
+
   return (
     <nav className="w-full h-12 flex justify-around items-center p-4 bg-amber-100">
-      <h1>Exclusive</h1>
+      <h1 className="text-lg font-bold">Exclusive</h1>
 
-      <ul className="flex bg-red-400">
+      <ul className="flex gap-4 bg-red-400">
         {menulist.map((item) => (
-          <li key={item.href}>
-            <Link href={item.href}>{item.label}</Link>
+          <li key={item.href} className="">
+            <Link href={item.href} className={pathname===item.href?"border-b-2 border-gray-300":""}>{item.label}</Link>
           </li>
         ))}
       </ul>
 
-      <div className="flex bg-gray-300">
+     <div className="flex gap-8">
+       <div className="flex bg-[#F5F5F5] opacity-70 rounded-md p-1  ">
         <input
-          className="border border-solid border-fuchsia-600"
+          className="outline-none border-none"
+          placeholder="What are you looking for?"
           type="text"
           name=""
           id=""
+          value={fieldText}
+          onChange={(e)=>setFieldText(e.target.value)}
         />
-        <SearchIcon />
+        <SearchIcon onClick={filterProducts}/>
       </div>
       <div className="flex bg-green-300">
         <Heart />
@@ -40,6 +54,7 @@ export default function Navbar() {
         <User />
         <Menu />
       </div>
+     </div>
     </nav>
   );
 }
