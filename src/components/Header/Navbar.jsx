045f4138a8d86be9b1ac53a4735a03fc3429d70b
Basit Ali <opoali911@gmail.com>
2025-08-16 19:11:15 +0500

    working in navbar component

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,54 +1,44 @@
-import { Heart, SearchIcon, ShoppingCart } from 'lucide-react'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
-import React, { useState } from 'react'
+import { Heart, SearchIcon, ShoppingCart } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import React, { useState } from "react";
 
 export default function Navbar() {
-
-  const [toggle ,setToggle]=useState(false)
-  const pathname=usePathname()
-const menulist=[
-  {href:"/",label:"Home"},
-  {href:"/contact",label:"Contact"},
-  {href:"/about",label:"About"},
-  {href:"/signup",label:"SignUp"},
-
-]
-
-
+  const [toggle, setToggle] = useState(false);
+  const pathname = usePathname();
+  const menulist = [
+    { href: "/", label: "Home" },
+    { href: "/contact", label: "Contact" },
+    { href: "/about", label: "About" },
+    { href: "/signup", label: "SignUp" },
+  ];
 
   return (
-    <nav className='flex bg-amber-300'>
-      <div>
-        <h1>Exclusive</h1>
-      </div>
-      <div>
-        <ul>
-        {
-          menulist.map((item)=>(
-            <li key={item.href}>
-              <Link href={item.href}>{item.label}</Link>
-            </li>
-          ))
-        }
-        </ul>
+    <nav className="flex bg-amber-300">
+      <h1>Exclusive</h1>
 
-      </div>
+      <ul>
+        {menulist.map((item) => (
+          <li key={item.href}>
+            <Link href={item.href}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+
       <div>
         <input type="text" name="" id="" />
-        <SearchIcon/>
+        <SearchIcon />
       </div>
       <div>
-        {
-          pathname==='/signup' || pathname==='/signin'?<>
-          </>:<><Heart/>
-        <ShoppingCart/></>
-        }
-
+        {pathname === "/signup" || pathname === "/signin" ? (
+          <></>
+        ) : (
+          <>
+            <Heart />
+            <ShoppingCart />
+          </>
+        )}
       </div>
-
-
-      
     </nav>
-  )
+  );
 }
