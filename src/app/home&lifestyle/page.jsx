f9779a73a44104a2ef36878e4ad11de39debb98f
Basit Ali <opoali911@gmail.com>
2025-08-17 21:31:54 +0500

    working  slider component of the comonent

---
+++ src/app/home&lifestyle/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomeAndLifestyle() {
+  return (
+    <div>
+      
+      <h1>home&lifestyle</h1>
+    </div>
+  )
+}
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,7 +4,12 @@ const sliderMenuList=[
     {href:"/womenfashion",label:"Women's Fashion"},
     {href:"/menfashion",label:"Men's Fashion"},
     {href:"/electronics",label:"Electronics"},
-    {href:"/home",label:"Home & Lifestyle"},
+    {href:"/home&lifestyle",label:"Home & Lifestyle"},
+    {href:"/medicine",label:"Medicine"},
+    {href:"/sports&Outdoor",label:"Sports & Outdoor"},
+    {href:"/baby&toys",label:"Baby & Toys"},
+    {href:"/groceries&pets",label:"Groceries & Pets"},
+    {href:"/health&beauty",label:"Health & Beauty"},
 ]
 
 export default function Slider() {
