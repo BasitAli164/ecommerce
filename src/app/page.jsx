8cdc8beae9cc8721b82b08c3b1cc2c149acec2d4
Basit Ali <opoali911@gmail.com>
2025-08-17 21:57:30 +0500

    working in hero component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 'use client'
+import Hero from "@/components/Hero/Hero"
 import Slider from "@/components/Slider/Slider"
 
 
 function Home() {
   return (
-    <div className="">
+    <div className="flex gap-14">
       <Slider/>
+      <Hero/>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 export default function Hero() {
   return (
-   <div>
-    
+   <div className='w-[892px] h-[344px] bg-bgPrimary mt-10'>
+    <div>
+      <div>
+        
+      </div>
+      <div>
+
+      </div>
+    </div>
+    <div>
+      <input type="radio" name="" id="" />
+      <input type="radio" name="" id="" />
+      <input type="radio" name="" id="" />
+    </div>
    </div>
   )
 }
