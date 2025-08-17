b3da127f31950af51a43c7dad20c3de589ea2cea
Basit Ali <opoali911@gmail.com>
2025-08-17 21:49:30 +0500

    now start to work in hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
-      <h1 className='text-5xl mt-10'> Hello</h1>
-    </div>
+   <div>
+    
+   </div>
   )
 }
