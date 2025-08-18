accf19a3eef9e48e7be43a967dbbd537b5c40838
Basit Ali <opoali911@gmail.com>
2025-08-18 23:20:13 +0500

    working in hero component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
 
 
   return (
-    <header className="w-full border-b border-gray-400 border-solid  ">
+    <header className="w-full border-b border-gray-200 border-solid  ">
       <AnnouncementBar />
       <Navbar/>
     </header>
