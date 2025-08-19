2f6c77b2c9a173478b5a02ad4228039ec93fa679
Basit Ali <opoali911@gmail.com>
2025-08-19 23:52:52 +0500

    working in nav bar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,38 +1,41 @@
-import { HeartIcon, SearchIcon, ShoppingCart } from 'lucide-react'
-const menuList=[
-    {href:"/",lable:"Home"},
-    {href:"/contact",lable:"Contact"},
-    {href:"/about",lable:"About"},
-    {href:"/signup",lable:"Sign Up"},
-]
+import { HeartIcon, SearchIcon, ShoppingCart } from "lucide-react";
+import Link from "next/link";
+const menuList = [
+  { href: "/", lable: "Home" },
+  { href: "/contact", lable: "Contact" },
+  { href: "/about", lable: "About" },
+  { href: "/signup", lable: "Sign Up" },
+];
 export default function Navbar() {
-    
   return (
-    <nav>
+    <nav className="w-full bg-amber-400 flex flex-col justify-center items-center">
+      <div className="w-full bg-red-600 flex justify-between items-center">
         <div>
-            <div>
-                <h1>Exclusive</h1>
-            </div>
-            <div>
-                <div>
-                    <input type="text" name="" id="" />
-                    <SearchIcon/>
-                </div>
-                <div>
-                    <ShoppingCart/>
-                    <HeartIcon/>
-
-                </div>
-            </div>
+          <h1>Exclusive</h1>
         </div>
-        <div>
-            <ul>
-                {
-                    menuList
-                }
-            </ul>
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="border-2 border-white border-solid"
+          />
+          <SearchIcon />
         </div>
-      
+        <div className="flex justify-center items-center">
+          <ShoppingCart />
+          <HeartIcon />
+        </div>
+      </div>
+      <div>
+        <ul className="flex">
+          {menuList.map((item) => (
+            <li key={item}>
+              <Link href={item.href}>{item.lable}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
