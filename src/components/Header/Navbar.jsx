53710a4f4810c73ef6134ebe03730891e9f72f03
Basit Ali <opoali911@gmail.com>
2025-08-20 00:20:12 +0500

    working in navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,5 +1,6 @@
 import { HeartIcon, SearchIcon, ShoppingCart, User } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 const menuList = [
   { href: "/", lable: "Home" },
   { href: "/contact", lable: "Contact" },
@@ -7,35 +8,37 @@ const menuList = [
   { href: "/signup", lable: "Sign Up" },
 ];
 export default function Navbar() {
+
+    const pathname=usePathname()
   return (
     <nav className="w-full bg-amber-400 flex flex-col justify-center items-center gap-1">
       <div className="w-full bg-red-600 flex justify-around items-center py-2">
-       
-          <h1 className="font-bold text-lg sm:text-xl md:text-3xl">Exclusive</h1>
-        
+        <h1 className="font-bold text-lg sm:text-xl md:text-3xl">Exclusive</h1>
+        <div>
+          <ul className="flex justify-center items-center gap-2">
+            {menuList.map((item) => (
+              <li key={item} className="text-lg sm:text-xl">
+                <Link href={item.href} className={pathname===item.href?"underline ":""}>{item.lable}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
         <div className="flex justify-center items-center gap-3">
-          <ShoppingCart className="size-[16px] sm:size-[20px] md:size-[28px]" />
+          <SearchIcon className="size-[16px] sm:size-[20px] md:size-[28px]" />
           <HeartIcon className="size-[16px] sm:size-[20px] md:size-[28px]" />
+          <ShoppingCart className="size-[16px] sm:size-[20px] md:size-[28px]" />
           <User className="size-[16px] sm:size-[20px] md:size-[28px]" />
         </div>
       </div>
       <div className="flex justify-center items-center">
-          <input
-            type="text"
-            name=""
-            id=""
-            className=" max-w-54 border-2 border-white border-solid"
-          />
-          <SearchIcon />
-        </div>
-      <div>
-        <ul className="flex">
-          {menuList.map((item) => (
-            <li key={item}>
-              <Link href={item.href}>{item.lable}</Link>
-            </li>
-          ))}
-        </ul>
+        <input
+          type="text"
+          name=""
+          id=""
+          className=" max-w-54 border-2 border-white border-solid"
+        />
+        <SearchIcon />
       </div>
     </nav>
   );
