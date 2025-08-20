7b6261bdec7edf6248abd3cd123615ba59248f88
Basit Ali <opoali911@gmail.com>
2025-08-20 22:22:27 +0500

    working in announcemet bar styling

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -3,22 +3,22 @@ import React from "react";
 
 export default function AnnouncementBar() {
   return (
-    <div className="w-full h-[48px] bg-bgPrimary ">
-      <div className="container max-w-[1400px] h-full flex flex-col sm:flex-row justify-around items-center px-4">
-        <div className="flex gap-x-2">
-          <p className="font-poppins text-secondaryText  text-sm md:text-base ">
+    <div className="w-full h-auto sm:h-[48px] bg-bgPrimary ">
+      <div className="container max-w-[1400px] h-full flex flex-col sm:flex-row justify-around items-center gap-y-2 px-4 ">
+        <div className="flex flex-col sm:flex-row justify-center items-center gap-x-2">
+          <p className="font-poppins text-secondaryText  text-sm md:text-base text-center ">
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
           </p>
             <Link className="underline text-secondaryText font-semibold text-sm md:text-base" href={"#"}>ShopNow</Link>
          
         </div>
-        <div className="max-w-10">
-          <select name="language" id="language" className="text-secondaryText outline-none">
-            <option value="en-US">English (United States)</option>
-            <option value="en-GB">English (United Kingdom)</option>
-            <option value="en-CA">English (Canada)</option>
-            <option value="en-AU">English (Australia)</option>
-            <option value="en-NZ">English (New Zeeland)</option>
+        <div className=" flex flex-col justify-center items-center gap-y-4">
+          <select name="language" id="language" className="text-secondaryText  outline-none rounded py-2 px-3 pr-8">
+            <option value="en-US" className="text-primaryText text-sm">English (US)</option>
+            <option value="en-GB" className="text-primaryText text-sm">English (UK)</option>
+            <option value="en-CA" className="text-primaryText text-sm">English (CA)</option>
+            <option value="en-AU" className="text-primaryText text-sm">English (AU)</option>
+            <option value="en-NZ" className="text-primaryText text-sm">English (NZ)</option>
           </select>
         </div>
       </div>
