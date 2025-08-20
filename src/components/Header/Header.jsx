13c4cddc782f8839edb403dadbb0453394e1d03f
Basit Ali <opoali911@gmail.com>
2025-08-21 00:01:40 +0500

    explore new concepts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,60 @@
-'use client'
-import  { useState } from 'react'
-import AnnouncementBar from './AnnouncementBar'
-import Link from 'next/link'
-import { Heart, Search, ShoppingCart } from 'lucide-react'
+"use client";
+import { useState } from "react";
+import AnnouncementBar from "./AnnouncementBar";
+import Link from "next/link";
+import { Heart, Search, ShoppingCart } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
-    const [searchText, setSearchText]=useState("")
-    console.log(searchText)
+  const [searchText, setSearchText] = useState("");
+  const pathname = usePathname();
+  const links = [
+    { href: "/", label: "Home" },
+    { href: "/contact", label: "Contact" },
+    { href: "/about", label: "About" },
+    { href: "/signup", label: "SignUp" },
+  ];
+  console.log(searchText);
   return (
-    <header className='w-full'>
-    <AnnouncementBar/>
-    <nav className='w-full bg-red-700 container  flex justify-around items-center py-5'>
-        <div className='max-w-[1000px] border-2 border-black border-dashed  bg-amber-300 flex justify-between items-center gap-32'>
-            <h1 className='text-3xl font-semibold'>Exclusive</h1>
-            <ul className='flex justify-center items-center gap-6'>
-                <li><Link href={'/'}>Home</Link></li>
-                <li><Link href={'/contact'}>Contact</Link></li>
-                <li><Link href={'/about'}>About</Link></li>
-                <li><Link href={'/signup'}>Sign Up</Link></li>
-                
-            </ul>
+    <header className="w-full">
+      <AnnouncementBar />
+      <nav className="w-full bg-red-700 container  flex justify-around items-center py-5">
+        <div className="max-w-[1000px] border-2 border-black border-dashed  bg-amber-300 flex justify-between items-center gap-32">
+          <h1 className="text-2xl font-semibold tracking-[1px]">Exclusive</h1>
+          <ul className="flex justify-center items-center gap-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={
+                    pathname === link.href
+                      ? "underline font-semibold text-primaryText"
+                      : "hover:underline"
+                  }
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className='flex justify-center items-center gap-10 border-4 border-green-400'>
-            <div className='flex justify-center items-center'>
-                <input className='border-2 border-black  outline-none ' type="text" name="searchText" id="searchText" onChange={(e)=>setSearchText(e.target.value)}/>
-                <Search className='w-6'/>
-            </div>
-            <div className='flex'>
-                <Heart className='w-6 '/>
-                <ShoppingCart className='w-6'/>
-            </div>
+        <div className="flex justify-center items-center gap-10 border-4 border-green-400">
+          <div className="flex justify-center items-center">
+            <input
+              className="border-2 border-black  outline-none "
+              type="text"
+              name="searchText"
+              id="searchText"
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <Search className="w-6" />
+          </div>
+          <div className="flex">
+            <Heart className="w-6 " />
+            <ShoppingCart className="w-6" />
+          </div>
         </div>
-
-    </nav>
-      
+      </nav>
     </header>
-  )
+  );
 }
