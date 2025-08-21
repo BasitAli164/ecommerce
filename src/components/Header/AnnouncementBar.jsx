e491495ff3e915253ce3ea54cc17e02c5997a6de
Basit Ali <opoali911@gmail.com>
2025-08-21 22:05:26 +0500

    working in styleing

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -4,15 +4,15 @@ import React from "react";
 export default function AnnouncementBar() {
   return (
     <div className="w-full h-[48px] bg-bgPrimary ">
-      <div className="container h-full max-w-[1600px] flex justify-around items-center px-4">
-        <div className="flex gap-x-4">
-          <p className="font-poppins text-secondaryText  text-sm md:text-base truncate">
+      <div className="container max-w-[1400px] h-full flex flex-col sm:flex-row justify-around items-center px-4">
+        <div className="flex gap-x-2">
+          <p className="font-poppins text-secondaryText  text-sm md:text-base ">
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
           </p>
             <Link className="underline text-secondaryText font-semibold text-sm md:text-base" href={"#"}>ShopNow</Link>
          
         </div>
-        <div>
+        <div className="max-w-10">
           <select name="language" id="language" className="text-secondaryText outline-none">
             <option value="en-US">English (United States)</option>
             <option value="en-GB">English (United Kingdom)</option>
