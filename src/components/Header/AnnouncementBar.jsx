56fe03356994b7b5bfe7d2cdd04357ddc2af5aaa
Basit Ali <opoali911@gmail.com>
2025-08-21 21:43:10 +0500

    working resposiveness of the announcement bar commpeonent

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 export default function AnnouncementBar() {
   return (
-    <div className="w-full h-[48px] bg-bgPrimary ">
+    <div className="w-full h-[48px] py-2 bg-bgPrimary ">
       <div className="container max-w-[1400px] flex justify-around items-center">
-        <div className="flex gap-2">
+        <div className="flex gap-x-4">
           <p className="font-poppins text-secondaryText">
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
           </p>
-            <Link className="underline text-secondaryText" href={"#"}>ShopNow</Link>
+            <Link className="underline text-secondaryText font-semibold" href={"#"}>ShopNow</Link>
          
         </div>
         <div>
