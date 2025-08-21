9bbea1c0cf04056fc015cac1937bf7590d57c809
Basit Ali <opoali911@gmail.com>
2025-08-21 21:55:06 +0500

    working in announcement component

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 export default function AnnouncementBar() {
   return (
-    <div className="w-full h-[48px] py-2 bg-bgPrimary ">
-      <div className="container max-w-[1400px] flex justify-around items-center">
+    <div className="w-full h-[48px] bg-bgPrimary ">
+      <div className="container h-full max-w-[1600px] flex justify-around items-center px-4">
         <div className="flex gap-x-4">
-          <p className="font-poppins text-secondaryText">
+          <p className="font-poppins text-secondaryText  text-sm md:text-base truncate">
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
           </p>
-            <Link className="underline text-secondaryText font-semibold" href={"#"}>ShopNow</Link>
+            <Link className="underline text-secondaryText font-semibold text-sm md:text-base" href={"#"}>ShopNow</Link>
          
         </div>
         <div>
