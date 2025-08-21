608ce102961faf7a92a66c421ed03ac3cf6dbd5e
Basit Ali <opoali911@gmail.com>
2025-08-21 10:44:40 +0500

    know start tow work styling of the annoucement bar

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -1,27 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function AnnouncementBar() {
   return (
     <div>
+      <div>
         <div>
-        <div>
-            <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
-            <Link href={'#'}>ShopNow</Link>
+          <p>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          </p>
+          <Link href={"#"}>ShopNow</Link>
         </div>
         <div>
-           <select name="language" id="language">
+          <select name="language" id="language">
             <option value="en-US">English (United States)</option>
             <option value="en-GB">English (United Kingdom)</option>
             <option value="en-CA">English (Canada)</option>
             <option value="en-AU">English (Australia)</option>
             <option value="en-NZ">English (New Zeeland)</option>
-            
-           </select>
-            
+          </select>
         </div>
-      
+      </div>
     </div>
-    </div>
-  )
+  );
 }
