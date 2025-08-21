721d1e2facf582526373959737ee1bb42c31d0d1
Basit Ali <opoali911@gmail.com>
2025-08-21 10:33:05 +0500

    working in announcementBar component

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function AnnouncementBar() {
   return (
     <div>
-        
+        <div>
+            <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
+            <Link href={'#'}>ShopNow</Link>
+        </div>
+        <div>
+            
+        </div>
       
     </div>
   )
