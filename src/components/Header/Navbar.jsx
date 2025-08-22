c034274869336895590bd6dedf21cef5945aab6c
Basit Ali <opoali911@gmail.com>
2025-08-22 22:26:15 +0500

    again start from zero means restart

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { HeartIcon, SearchIcon, ShoppingBag, ShoppingCart } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+    const [searchField,setSearchField]=useState("")
+    const pathname=usePathname()
+
+
+    const menuList=[
+        {href:"/",label:"Home"},
+        {href:"/contact",label:"Contact"},
+        {href:"/about",label:"About"},
+        {href:"/signup",label:"Sign Up"},
+    ]
+    
   return (
-    <nav className='w-screen h-20 bg-amber-500'>
+    <nav className='w-screen h-20 flex justify-between items-center p-4 bg-amber-500'>
+        <div>
+            <h1>Exclusiv</h1>
+        </div>
+        <ul className='flex'>
+            {
+                menuList.map((item)=>(
+                    <li key={item.label}><Link className={pathname===item.href?"underline ":""} href={item.href}>{item.label}</Link></li>
+                ))
+            }
+
+        </ul>
+        <div>
+            <input type="text" name="searchfield" id="searchfield" onChange={(e)=>setSearchField(e.target.value)} className='border border-white border-solid' />
+            <SearchIcon/>
+        </div>
+        <div>
+            <ShoppingCart/>
+            <HeartIcon/>
+        </div>
 
 
       
