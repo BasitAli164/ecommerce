9b61b213f372a92bff2bf818925ce4c7065bfe6f
Basit Ali <opoali911@gmail.com>
2025-08-22 20:45:20 +0500

    again styling the header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,35 +8,31 @@ import { usePathname } from "next/navigation";
 export default function Header() {
   const [searchText, setSearchText] = useState("");
   const pathname = usePathname();
+
   const links = [
     { href: "/", label: "Home" },
     { href: "/contact", label: "Contact" },
     { href: "/about", label: "About" },
     { href: "/signup", label: "SignUp" },
   ];
-  console.log(searchText);
 
-
-  const handleSearchData=(searchData)=>{
-    console.log("Search Data",searchData)
+  const handleSearchData = (searchData) => {
+    console.log("Search Data", searchData);
     /* work after making api*/
-  }
+  };
+
   return (
-    <header className="">
+    <header>
       <AnnouncementBar />
-      <nav className="w-full bg-red-700   flex justify-around items-center py-5">
-        <div className="w-full max-w-[1000px] border-2 border-black border-dashed  bg-amber-300 flex justify-between items-center gap-32">
-          <h1 className="text-2xl font-semibold tracking-[1px]">Exclusive</h1>
-          <ul className="flex justify-center items-center gap-6">
+      <nav className="w-full bg-red-400 flex  justify-between items-center px-8 ">
+        <div className="flex gap-20">
+          <h1>Exclusive</h1>
+          <ul className="flex gap-5">
             {links.map((link) => (
               <li key={link.href}>
                 <Link
                   href={link.href}
-                  className={
-                    pathname === link.href
-                      ? "underline font-semibold text-primaryText"
-                      : "hover:underline"
-                  }
+                  className={pathname === link.href ? "active-link" : ""}
                 >
                   {link.label}
                 </Link>
@@ -44,20 +40,19 @@ export default function Header() {
             ))}
           </ul>
         </div>
-        <div className="flex justify-center items-center gap-10 border-4 border-green-400">
-          <div className="flex justify-center items-center border-1 p-1 border-white border-dashed">
+        <div>
+          <div>
             <input
-              className="  outline-none "
               type="text"
               name="searchText"
               id="searchText"
               onChange={(e) => setSearchText(e.target.value)}
             />
-            <Search className="w-6" onClick={()=>handleSearchData(searchText)}/>
+            <Search onClick={() => handleSearchData(searchText)} />
           </div>
-          <div className="flex">
-            <Heart className="w-6 " />
-            <ShoppingCart className="w-6" />
+          <div>
+            <Heart />
+            <ShoppingCart />
           </div>
         </div>
       </nav>
