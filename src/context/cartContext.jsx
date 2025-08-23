a8110d6430b7f9b86dfd9b285866b64ed5111027
Basit Ali <opoali911@gmail.com>
2025-08-24 00:21:37 +0500

    create the cart context

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -4,6 +4,29 @@ import { cartsData } from "@/data/dummyData";
 
 // Create Context
 
-export const cartContext=createContext();
+export const CartContext=createContext();
+
+// Create Provider
+
+export const CartProvider=({children})=>{
+    const [cartData,setCartData]=useState(cartsData);
+
+    // add function when dynmically doing work
+
+
+    return (
+        <CartContext value={{cartData}}>
+            {children}
+        </CartContext>
+
+    )
+
+}
+
+// Create Custom Hook
+
+export const useCart=()=>{
+    return useContext(CartContext)
+}
 
 
