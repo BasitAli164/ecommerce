66a7c333e28d8de5e5fa1f5be26ce52ba6445afa
Basit Ali <opoali911@gmail.com>
2025-08-23 23:24:47 +0500

    rating component is ready

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -1,10 +1,33 @@
+import { Star } from 'lucide-react'
 import React from 'react'
 
 export default function Rating(value,max=5) {
-    
+    const stars=[]
+
+    for(i=1;i<=max;i++){
+        if(i<=Math.floor(value)){
+            stars.push(
+                <Star
+                key={i}
+                fill='currentColor'
+                stroke='none'
+                className='w-5 h-5 text-yellow-500'
+                
+                />
+            )
+        }else if(i-value<i && i>value){
+            stars.push(
+                <Star
+                key={i}
+                className='w-5 h-5 text-yellow-500'
+                />
+            )
+        }
+    }
+
   return (
-    <div>
-      
+    <div className='flex items-center gap-1'>
+      {stars}
     </div>
   )
 }
