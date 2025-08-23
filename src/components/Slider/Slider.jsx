2fb9c3063f4f9e622f4584b933c3435772bdd4a5
Basit Ali <opoali911@gmail.com>
2025-08-23 23:30:38 +0500

    work in cart component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import Rating from "../rating/Rating";
 
 const sliderMenuList = [
   { href: "/womenfashion", label: "Women's Fashion" },
@@ -33,7 +32,6 @@ export default function Slider() {
         </p>
       ))}
 
-      <Rating value={3}/>
     </div>
   );
 }
