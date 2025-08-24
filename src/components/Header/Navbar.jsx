dd038dd1f16389fdc5b7a6aa2b6e197ee699c02f
Basit Ali <opoali911@gmail.com>
2025-08-24 14:04:16 +0500

    stuck

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import { HeartIcon, Menu, SearchIcon, ShoppingCart, User } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -10,29 +9,31 @@ const menuList = [
   { href: "/signup", lable: "Sign Up" },
 ];
 export default function Navbar() {
-      const [togle,setToggle]=useState(false)
+  const [togle, setToggle] = useState(false);
+  const handle=()=>{
+    console.log(togle)
+    console.log("object")
+    setToggle(!togle)
+  }
 
-
-
- 
-
-    const pathname=usePathname()
+  const pathname = usePathname();
   return (
     <nav className="w-full bg-amber-400 flex flex-col justify-center items-center gap-1">
       <div className="w-full bg-red-600 flex justify-around items-center py-2">
         <h1 className="font-bold text-lg sm:text-xl md:text-3xl">Exclusive</h1>
         <div className="hidden sm:block">
-        
-            
-              <ul className="flex justify-center items-center gap-4">
+          <ul className="flex justify-center items-center gap-4">
             {menuList.map((item) => (
               <li key={item} className="text-lg sm:text-xl">
-                <Link href={item.href} className={pathname===item.href?"underline ":""}>{item.lable}</Link>
+                <Link
+                  href={item.href}
+                  className={pathname === item.href ? "underline " : ""}
+                >
+                  {item.lable}
+                </Link>
               </li>
             ))}
           </ul>
-            
-        
         </div>
 
         <div className="flex justify-center items-center gap-3 relative">
@@ -40,24 +41,26 @@ export default function Navbar() {
           <HeartIcon className="size-[16px] sm:size-[20px] md:size-[24px]" />
           <ShoppingCart className="size-[16px] sm:size-[20px] md:size-[24px]" />
           <User className="size-[16px] sm:size-[20px] md:size-[28px]" />
-          <Menu className="sm:hidden size-[16px] sm:size-[20px] md:size-[24px]" onClick={()=>setToggle(!togle)}/>
-         <div className=" absolute top-0 -right-12 p-8 text-center sm:hidden">
-            
-             
-               <ul>
-                 {menuList.map((item) => (
-              <li key={item} className="text-lg sm:text-xl">
-                <Link href={item.href} className={pathname===item.href?"underline ":""}>{item.lable}</Link>
-              </li>
-            ))}
-              </ul>
-             
-            
+          <Menu
+            className="sm:hidden size-[16px] sm:size-[20px] md:size-[24px]"
+            onClick={handle}
+          />
+          <div className=" absolute top-0 -right-12 p-8 text-center sm:hidden">
+            <ul>
+              {menuList.map((items) => (
+                <li key={items} className="text-lg sm:text-xl">
+                  <Link
+                    href={items.href}
+                    className={pathname === items.href ? "underline " : ""}
+                  >
+                    {items.lable}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
-
-      
     </nav>
   );
 }
