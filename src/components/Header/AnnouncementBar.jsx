9caf03fbfe3ac21f1e6cfb3049f4d232e9592cfb
Basit Ali <opoali911@gmail.com>
2025-08-24 14:45:27 +0500

    again restart

--- src/components/Header/AnnouncementBar.jsx
+++ src/components/Header/AnnouncementBar.jsx
@@ -1,27 +1,9 @@
-import Link from "next/link";
-import React from "react";
+import React from 'react'
 
 export default function AnnouncementBar() {
   return (
-    <section className="w-full  sm:h-[48px] bg-bgPrimary ">
-      <div className="container max-w-[1400px] h-full flex flex-col sm:flex-row justify-around items-center gap-y-0.5 px-4 ">
-        <div className="flex flex-col sm:flex-row justify-center items-center gap-x-2">
-          <p className="font-poppins text-secondaryText  text-sm md:text-base text-center ">
-            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-          </p>
-            <Link className="underline text-secondaryText font-semibold text-sm md:text-base" href={"#"}>ShopNow</Link>
-         
-        </div>
-        <div className=" hidden sm:flex flex-col justify-center items-center gap-y-4">
-          <select name="language" id="language" className="text-secondaryText  outline-none rounded py-2 px-3 md:pr-4">
-            <option value="en-US" className="text-primaryText text-sm">English (US)</option>
-            <option value="en-GB" className="text-primaryText text-sm">English (UK)</option>
-            <option value="en-CA" className="text-primaryText text-sm">English (CA)</option>
-            <option value="en-AU" className="text-primaryText text-sm">English (AU)</option>
-            <option value="en-NZ" className="text-primaryText text-sm">English (NZ)</option>
-          </select>
-        </div>
-      </div>
-    </section>
-  );
+    <div>
+      
+    </div>
+  )
 }
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,66 +1,9 @@
-import { HeartIcon, Menu, SearchIcon, ShoppingCart, User } from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-const menuList = [
-  { href: "/", lable: "Home" },
-  { href: "/contact", lable: "Contact" },
-  { href: "/about", lable: "About" },
-  { href: "/signup", lable: "Sign Up" },
-];
+import React from 'react'
+
 export default function Navbar() {
-  const [togle, setToggle] = useState(false);
-  const handle=()=>{
-    console.log(togle)
-    console.log("object")
-    setToggle(!togle)
-  }
-
-  const pathname = usePathname();
   return (
-    <nav className="w-full bg-amber-400 flex flex-col justify-center items-center gap-1">
-      <div className="w-full bg-red-600 flex justify-around items-center py-2">
-        <h1 className="font-bold text-lg sm:text-xl md:text-3xl">Exclusive</h1>
-        <div className="hidden sm:block">
-          <ul className="flex justify-center items-center gap-4">
-            {menuList.map((item) => (
-              <li key={item} className="text-lg sm:text-xl">
-                <Link
-                  href={item.href}
-                  className={pathname === item.href ? "underline " : ""}
-                >
-                  {item.lable}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="flex justify-center items-center gap-3 relative">
-          <SearchIcon className="size-[16px] sm:size-[20px] md:size-[24px]" />
-          <HeartIcon className="size-[16px] sm:size-[20px] md:size-[24px]" />
-          <ShoppingCart className="size-[16px] sm:size-[20px] md:size-[24px]" />
-          <User className="size-[16px] sm:size-[20px] md:size-[28px]" />
-          <Menu
-            className="sm:hidden size-[16px] sm:size-[20px] md:size-[24px]"
-            onClick={handle}
-          />
-          <div className=" absolute top-0 -right-12 p-8 text-center sm:hidden">
-            <ul>
-              {menuList.map((items) => (
-                <li key={items} className="text-lg sm:text-xl">
-                  <Link
-                    href={items.href}
-                    className={pathname === items.href ? "underline " : ""}
-                  >
-                    {items.lable}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+    <div>
+      
+    </div>
+  )
 }
