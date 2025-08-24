906c1b97bd7c79b371326b39a08102aad53725cd
Basit Ali <opoali911@gmail.com>
2025-08-24 13:42:36 +0500

    fix the issue

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -10,46 +10,19 @@ const menuList = [
   { href: "/signup", lable: "Sign Up" },
 ];
 export default function Navbar() {
-      const [innerWidth,setInnerWidth]=useState(0)
       const [togle,setToggle]=useState(true)
 
 
       console.log(innerWidth)
 
-  useEffect(()=>{
-    const handleInnerWidth=()=>{
-
-      const width= window.innerWidth
-      if(width<640){
-
-       
-        setInnerWidth(width)
-        setToggle(false)
-        console.log(width)
-
-      }else{
-        setToggle(true)
-
-      }
-
-    }
-
-    handleInnerWidth()
-    
-
-    window.addEventListener('resize',handleInnerWidth)
-
-    return ()=>removeEventListener("resize",handleInnerWidth)
-
-
-  },[innerWidth])
+ 
 
     const pathname=usePathname()
   return (
     <nav className="w-full bg-amber-400 flex flex-col justify-center items-center gap-1">
       <div className="w-full bg-red-600 flex justify-around items-center py-2">
         <h1 className="font-bold text-lg sm:text-xl md:text-3xl">Exclusive</h1>
-        <div>
+        <div className="hidden sm:block">
           {
             togle &&(
               <ul className="flex justify-center items-center gap-4">
@@ -63,14 +36,26 @@ export default function Navbar() {
           }
         </div>
 
-        <div className="flex justify-center items-center gap-3">
+        <div className="flex justify-center items-center gap-3 relative">
           <SearchIcon className="size-[16px] sm:size-[20px] md:size-[24px]" />
           <HeartIcon className="size-[16px] sm:size-[20px] md:size-[24px]" />
           <ShoppingCart className="size-[16px] sm:size-[20px] md:size-[24px]" />
           <User className="size-[16px] sm:size-[20px] md:size-[28px]" />
-          <Menu className="sm:hidden size-[16px] sm:size-[20px] md:size-[24px]"/>
+          <Menu className="sm:hidden size-[16px] sm:size-[20px] md:size-[24px]" onClick={()=>setToggle(!togle)}/>
+          <div className=" absolute top-5 -right-6 border-2 border-black border-solid p-8 text-center sm:hidden">
+            {
+              <ul>
+                 {menuList.map((item) => (
+              <li key={item} className="text-lg sm:text-xl">
+                <Link href={item.href} className={pathname===item.href?"underline ":""}>{item.lable}</Link>
+              </li>
+            ))}
+              </ul>
+            }
+          </div>
         </div>
       </div>
+
       
     </nav>
   );
