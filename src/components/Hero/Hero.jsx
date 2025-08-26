dc84988f2098af78e4de686b08eacae3fb2502d9
Basit Ali <opoali911@gmail.com>
2025-08-26 19:29:35 +0500

    working in hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
+    <div className='w-full max-w-[892px] h-[344px]  bg-amber-300'>
+      <div className='grid grid-cols-2 justify-center items-center'>
+        <div>
+          <img src="/hero/apple.png" alt="brand name" className='size-10' />
+        </div>
+
+      </div>
       
     </div>
   )
