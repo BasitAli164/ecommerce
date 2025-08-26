8022e5f9206eaafa90c5c3216bd8fb41c623e3c2
Basit Ali <opoali911@gmail.com>
2025-08-26 19:45:11 +0500

    again start to make the hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,9 @@
-import { MoveRight } from 'lucide-react'
-import Link from 'next/link'
 import React from 'react'
 
 export default function Hero() {
   return (
    <div>
-      <div>
-        
-      </div>
-      <div>
-      <input type="radio" name="radio" id="radio" className='accent-[#DB4444]'/>
-      <input type="radio" name="radio" id="radio" className='accent-[#DB4444]'/>
-      <input type="radio" name="radio" id="radio" defaultChecked className='accent-[#DB4444]'/>
-      <input type="radio" name="radio" id="radio" className='accent-[#DB4444]'/>
-      <input type="radio" name="radio" id="radio" className='accent-[#DB4444]'/>
-      </div>
+      
    </div>
   )
 }
