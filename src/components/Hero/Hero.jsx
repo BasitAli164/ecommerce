65f97b38922ed9bdf27c31e7c1dbc3de66d0b334
Basit Ali <opoali911@gmail.com>
2025-08-26 10:23:31 +0500

    working in hero component responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,32 +7,32 @@ import  { useState } from "react";
 export default function Hero() {
   const [data, setData] = useState(heroData);
   return (
-    <div className="max-w-[892px] w-full h-auto bg-bgPrimary mt-10 p-4 md:p-4 mb-10 md:mb-0 mx-auto rounded-sm">
+    <div>
       {data.map((item) => (
         <div
           key={item.id}
-          className="flex flex-col-reverse md:flex-row justify-between items-center flex-wrap"
+         
         >
-          <div className=" flex flex-col justify-center gap-4 ml-5">
-            <div className="flex gap-3 items-center">
+          <div >
+            <div>
               <Image
                 src={item.logoImage}
                 alt={item.brandName}
                 width={75}
                 height={75}
                 priority
-                className="object-contain"
+                
               />
-              <p className="text-sm md:text-lg text-secondaryText">
+              <p>
                 {item.brandName}
               </p>
             </div>
-            <h1 className="text-secondaryText text-lg md:text-4xl lg:text-6xl tracking-tight">
+            <h1>
              {item.title}
             </h1>
-            <div className="flex gap-2">
+            <div >
               <Link
-                className="text-secondaryText border-b border-gray-400 border-solid text-[12px] md:text-sm"
+                
                 href={""}
               >
                 Shop Now
