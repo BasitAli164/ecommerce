600ac6d421afd46652143eaf5354e29979ace8db
Basit Ali <opoali911@gmail.com>
2025-08-27 21:12:57 +0500

    working in flashSales component

--- src/components/CountDownTimer/CountDownTime.jsx
+++ src/components/CountDownTimer/CountDownTime.jsx
@@ -1,66 +1,66 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function CountDownTime() {
+  // target date  static
+  // const targetDate=new Date("2025-09-05T00:00:00") //! make sure use this when backend and DB integrate
 
-    // target date  static
-    // const targetDate=new Date("2025-09-05T00:00:00") //! make sure use this when backend and DB integrate
+  // set target date dynamically
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 3);
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
 
+  const calculateTimeLeft = () => {
+    const now = new Date().getTime();
+    const difference = targetDate.getTime() - now;
 
-    // set target date dynamically 
-    const targetDate=   new Date();
-    targetDate.setDate(targetDate.getDate()+3)
-    const [timeLeft,setTimeLeft]=useState({
-        days:0,
-        hours:0,
-        minutes:0,
-        seconds:0
-    })
-
-    const calculateTimeLeft=()=>{
-        const now=new Date().getTime();
-        const difference=targetDate.getTime()-now;
-
-        if(difference<0){
-            return  {days:0,hours:0,minutes:0,seconds:0}
-        }
-
-        return {
-            days: Math.floor(difference/(1000*60*60*60)),
-            hours: Math.floor((difference/(1000*60*60))%24),
-            minutes: Math.floor((difference/(1000*60))%60),
-            seconds: Math.floor((difference/1000)%60)
-        }
+    if (difference < 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
     }
 
-    useEffect(()=>{
-        const timer=setInterval(()=>{
-            setTimeLeft(calculateTimeLeft())
-        },1000)
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 60)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  };
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
 
-        return ()=> clearInterval(timer)
-    },[])
-
-
+    return () => clearInterval(timer);
+  }, []);
 
   return (
-    <div className='flex gap-4 text-center'>
-        <div>
-            <span>Days</span>
-            <p className='text-2xl font-bold'>{timeLeft.days}</p>
-        </div>
-        <div>
-            <span>Hours</span>
-            <p className='text-2xl font-bold'>{timeLeft.hours}</p>
-        </div>
-        <div>
-            <span>Minutes</span>
-            <p className='text-2xl font-bold'>{timeLeft.minutes}</p>
-        </div>
-        <div>
-            <span>Seconds</span>
-            <p className='text-2xl font-bold'>{timeLeft.seconds}</p>
-        </div>
+    <div className="flex gap-4 text-center">
+      <div>
+        <span>Days</span>
+        <p className="text-2xl font-bold">{timeLeft.days}</p>
+      </div>
+      <span className="mt-6 text-xl text-accent font-bold">:</span>
+
+      <div>
+        <span>Hours</span>
+        <p className="text-2xl font-bold">{timeLeft.hours}</p>
+      </div>
+      <span className="mt-6 text-xl text-accent font-bold">:</span>
+
+      <div>
+        <span>Minutes</span>
+        <p className="text-2xl font-bold">{timeLeft.minutes}</p>
+      </div>
+      <span className="mt-6 text-xl text-accent font-bold">:</span>
+      <div>
+        <span>Seconds</span>
+        <p className="text-2xl font-bold">{timeLeft.seconds}</p>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/FlashSales/FlashSales.jsx
+++ src/components/FlashSales/FlashSales.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import CountDownTime from '../CountDownTimer/CountDownTime'
 import { MoveLeft, MoveRight } from 'lucide-react'
 import Cart from '../cart/Cart'
 import { useCart } from '@/context/cartContext'
+import Link from 'next/link'
 
 
 export default function FlashSales() {
+  // Create a ref for the scrollable container
+  const scrollRef=useRef<HTMLDivElement>(null)
   const {cartData}=useCart()
+
+  // Functions to scroll left/right
+  
+  const scrollLeft=()=>{
+    if(scrollRef.current){
+      scrollRef.current.scrollBy({left:-300,behavior:"smooth"})
+    }
+  }
+  const scrollRight=()=>{
+    if(scrollRef.current){
+      scrollRef.current.scrollBy({left:-300,behavior:"smooth"})
+    }
+  }
+
+
   return (
-    <div className='max-w-[1170px] flex flex-col justify-center mx-auto mt-32'>
+    <div className='max-w-[1170px] flex flex-col justify-center mx-auto mt-32 mb-10' >
       <div className='flex justify-between items-center'>
       <div>
         <div className='flex items-center gap-2'>
@@ -21,13 +39,13 @@ export default function FlashSales() {
         </div>
 
       </div>
-      <div>
+      <div className='flex justify-center items-center gap-8'>
         <MoveLeft/>
         <MoveRight/>
       </div>
       
     </div>
-    <div className='flex gap-[30px]'>
+    <div className='flex gap-[30px] mt-10' >
       {
         cartData.map((item)=>(
           <Cart key={item.id} prodDetail={item}/>
@@ -36,8 +54,8 @@ export default function FlashSales() {
     </div>
 
 
-    <div>
-      <button className='px-10 py-4 bg-bgBtn text-center'>View All Products</button>
+    <div className='flex justify-center items-center'>
+      <Link href={""} className='px-10 py-3 bg-bgBtn text-center'>View All Products</Link>
     </div>
     </div>
   )
