2bfa473103fd77dda0aae639f1014d4c9903339f
Basit Ali <opoali911@gmail.com>
2025-08-27 20:48:02 +0500

    working in flashsales component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,29 +4,23 @@ import Hero from "@/components/Hero/Hero";
 import Slider from "@/components/Slider/Slider";
 import HeroProvider from "@/context/heroContext";
 import { useCart } from "@/context/cartContext";
+import FlashSales from "@/components/FlashSales/FlashSales";
 
 function Home() {
-  const {cartData}=useCart()
+  const { cartData } = useCart();
   return (
     <>
-    <div className="flex flex-col md:flex-row gap-2 md:gap-8 xl:gap-14">
-      <Slider/>
-      <div className="ml-8 sm:ml-0 w-10/12 flex justify-center items-center overflow-hidden mt-4">
-       <HeroProvider>
-         <Hero />
-       </HeroProvider>
+      <div className="flex flex-col md:flex-row gap-2 md:gap-8 xl:gap-14">
+        <Slider />
+        <div className="ml-8 sm:ml-0 w-10/12 flex justify-center items-center overflow-hidden mt-4">
+          <HeroProvider>
+            <Hero />
+          </HeroProvider>
+        </div>
       </div>
-    </div>
-     <div className=' flex flex-wrap justify-center items-center gap-5 overflow-hidden '>
-        {
-          cartData.map((item)=>(
-            <Cart key={item.id} prodDetail={item}/>
-          ))
-        }
+      <div>
+        <FlashSales />
       </div>
-   
-
-
     </>
   );
 }
