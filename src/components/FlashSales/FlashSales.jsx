a08d985c47f1be3431e97aace65270153b2b9097
Basit Ali <opoali911@gmail.com>
2025-08-27 19:57:07 +0500

    add one more color in global css and fix the issue in hero component

--- src/components/FlashSales/FlashSales.jsx
+++ src/components/FlashSales/FlashSales.jsx
@@ -8,7 +8,10 @@ export default function FlashSales() {
           <div className='w-5 h-8 bg-bgBtn '></div>
           <p className='text-accent font-bold text-xl'>Today's</p>
         </div>
-        
+        <div>
+          <h2>Flash Sales</h2>
+        </div>
+
       </div>
       
     </div>
