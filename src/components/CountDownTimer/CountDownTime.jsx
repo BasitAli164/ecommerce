abfaf0ec8d2985b502bc9e2f5cdae6a9ebc1b41a
Basit Ali <opoali911@gmail.com>
2025-08-27 20:23:42 +0500

    working in countDownTime component

--- src/components/CountDownTimer/CountDownTime.jsx
+++ src/components/CountDownTimer/CountDownTime.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
 
 export default function CountDownTime() {
-    const targetDate=new Date("2025-09-05T00:00:00")
+
+    // target date  static
+    // const targetDate=new Date("2025-09-05T00:00:00") //! make sure use this when backend and DB integrate
+
+
+    // set target date dynamically 
+    const targetDate=   new Date();
+    targetDate.setDate(targetDate.getDate()+3)
     const [timeLeft,setTimeLeft]=useState({
-        day:0,
+        days:0,
         hours:0,
         minutes:0,
         seconds:0
     })
-    
+
+
+
   return (
     <div>
       
