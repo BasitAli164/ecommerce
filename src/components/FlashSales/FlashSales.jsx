78eec9f36ab625ab19da6f641256c9bfd94132fd
Basit Ali <opoali911@gmail.com>
2025-08-27 21:47:59 +0500

    complete teh flashSales Component

--- src/components/FlashSales/FlashSales.jsx
+++ src/components/FlashSales/FlashSales.jsx
@@ -1,65 +1,63 @@
-  "use client"
-  import React,{useRef} from 'react'
-  import CountDownTime from '../CountDownTimer/CountDownTime'
-  import { ChevronLeft, ChevronRight,MoveLeft, MoveRight } from 'lucide-react'
-  import Cart from '../cart/Cart'
-  import { useCart } from '@/context/cartContext'
-  import Link from 'next/link'
+"use client";
+import React, { useRef } from "react";
+import CountDownTime from "../CountDownTimer/CountDownTime";
+import { ChevronLeft, ChevronRight, MoveLeft, MoveRight } from "lucide-react";
+import Cart from "../cart/Cart";
+import { useCart } from "@/context/cartContext";
+import Link from "next/link";
 
+export default function FlashSales() {
+  // Create a ref for the scrollable container
+  const scrollRef = useRef(null);
+  const { cartData } = useCart();
 
-  export default function FlashSales() {
-    // Create a ref for the scrollable container
-    const scrollRef=useRef(null)
-    const {cartData}=useCart()
+  // Functions to scroll left/right
 
-    // Functions to scroll left/right
-    
-    const scrollLeft=()=>{
-      if(scrollRef.current){
-        scrollRef.current.scrollBy({left:-300,behavior:"smooth"})
-      }
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
     }
-    const scrollRight=()=>{
-      if(scrollRef.current){
-        scrollRef.current.scrollBy({left:300,behavior:"smooth"})
-      }
+  };
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
     }
+  };
 
-
-    return (
-      <div className='w-full sm:max-w-[1170px] px-4 sm:px-6 lg:px-8 flex flex-col justify-center mx-auto mt-10 mb-5' >
-        <div className='flex justify-between items-center'>
+  return (
+    <div className="w-full sm:max-w-[1170px] px-4 sm:px-6 lg:px-8 flex flex-col justify-center mx-auto mt-10 mb-5">
+      <div className="flex justify-between items-center">
         <div>
-          <div className='flex items-center gap-2'>
-            <div className='w-3 h-8 bg-bgBtn '></div>
-            <p className='text-accent font-bold text-lg sm:text-2xl'>Today's</p>
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-8 bg-bgBtn "></div>
+            <p className="text-accent font-bold text-lg sm:text-2xl">Today's</p>
           </div>
-          <div className='flex flex-col sm:flex-row sm:gap-16 lg:gap-32 sm:mt-10'>
-            <h2 className='text-2xl sm:text-3xl lg:text-[36px] font-semibold'>Flash Sales</h2>
-            <CountDownTime/>
+          <div className="flex flex-col sm:flex-row sm:gap-16 lg:gap-32 sm:mt-10">
+            <h2 className="text-2xl sm:text-3xl lg:text-[36px] font-semibold">
+              Flash Sales
+            </h2>
+            <CountDownTime />
           </div>
-
         </div>
-        <div className='hidden sm:flex justify-around sm:justify-center items-center gap-4 sm:gap-6'>
-          <ChevronLeft onClick={scrollLeft}/>
-          <ChevronRight onClick={scrollRight}/>
+        <div className="hidden sm:flex justify-around sm:justify-center items-center gap-4 sm:gap-6">
+          <ChevronLeft onClick={scrollLeft} />
+          <ChevronRight onClick={scrollRight} />
         </div>
-        
       </div>
-      <div 
-      ref={scrollRef} // connect with ref here
-      className='flex gap-[30px] mt-10 overflow-x-auto scroll-smooth no-scrollbar' >
-        {
-          cartData.map((item)=>(
-            <Cart key={item.id} prodDetail={item}/>
-          ))
-        }
+      <div
+        ref={scrollRef} // connect with ref here
+        className="flex gap-[30px] mt-10 overflow-x-auto scroll-smooth no-scrollbar"
+      >
+        {cartData.map((item) => (
+          <Cart key={item.id} prodDetail={item} />
+        ))}
       </div>
 
-
-      <div className='flex justify-center items-center'>
-        <Link href={""} className='px-10 py-3 bg-bgBtn text-center'>View All Products</Link>
+      <div className="flex justify-center items-center">
+        <Link href={""} className="px-10 py-3 bg-bgBtn text-center">
+          View All Products
+        </Link>
       </div>
-      </div>
-    )
-  }
+    </div>
+  );
+}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,66 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { heroData } from "@/data/dummyData";
 import Link from "next/link";
-import { ChevronLeft, ChevronRight,MoveRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, MoveRight } from "lucide-react";
 import { useHero } from "@/context/heroContext";
 
-
 export default function Hero() {
+  const { index, handleNext, handlePrevious } = useHero();
 
-  const {index,handleNext,handlePrevious}=useHero()
-  
-
-  useEffect(()=>{
-    const interval=setInterval(() => {
-      handleNext()
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
     }, 30000);
 
-    return ()=>clearInterval(interval)
-  },[])
+    return () => clearInterval(interval);
+  }, []);
 
-
-  const item=heroData[index]
+  const item = heroData[index];
   return (
     <div className=" max-w-[90%] w-full  sm:p-5  rounded-sm shadow-2xl bg-bgPrimary text-secondaryText min-h-[400px]">
-      
-        <div
-          key={item.id}
-          className="w-full flex flex-col-reverse sm:flex-row sm:gap-10 overflow-hidden transition-all duration-500 ease-in-out"
-        >
-          <div className="flex flex-col gap-2 justify-center items-center min-h-[200px]">
-            <div className=" flex items-center">
-              <img
-                src={item.logoImage}
-                alt={item.brandName}
-                className="w-10 sm:w-16 "
-              />
-              <p className="text-12 sm:text-lg">{item.brandName}</p>
-            </div>
-            <div className="flex flex-col items-center  justify-center mb-10 sm:0 gap-4">
-              <h1 className="text-lg sm:text-4xl text-center">
-               {item.title}
-              </h1>
-              <div className="flex items-center gap-1">
-                <Link className="underline sm:text-lg" href={""}>
-                  Shop Now
-                </Link>
-                <MoveRight className="size-3 sm:size-5 mt-1" />
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center items-center min-h-[300px]">
+      <div
+        key={item.id}
+        className="w-full flex flex-col-reverse sm:flex-row sm:gap-10 overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <div className="flex flex-col gap-2 justify-center items-center min-h-[200px]">
+          <div className=" flex items-center">
             <img
-              src={item.mainImage}
+              src={item.logoImage}
               alt={item.brandName}
-              className="w-52 sm:w-[500px] h-[300px] object-contain "
+              className="w-10 sm:w-16 "
             />
+            <p className="text-12 sm:text-lg">{item.brandName}</p>
+          </div>
+          <div className="flex flex-col items-center  justify-center mb-10 sm:0 gap-4">
+            <h1 className="text-lg sm:text-4xl text-center">{item.title}</h1>
+            <div className="flex items-center gap-1">
+              <Link className="underline sm:text-lg" href={""}>
+                Shop Now
+              </Link>
+              <MoveRight className="size-3 sm:size-5 mt-1" />
+            </div>
           </div>
         </div>
-      
+        <div className="flex justify-center items-center min-h-[300px]">
+          <img
+            src={item.mainImage}
+            alt={item.brandName}
+            className="w-52 sm:w-[500px] h-[300px] object-contain "
+          />
+        </div>
+      </div>
 
-      <div className="flex justify-between mx-5 items-center ">
-        <ChevronLeft onClick={handlePrevious}  className="cursor-pointer"/>
-        <ChevronRight onClick={handleNext}  className="cursor-pointer"/>
+      <div className="hidden sm:flex justify-between mx-5 items-center ">
+        <ChevronLeft onClick={handlePrevious} className="cursor-pointer" />
+        <ChevronRight onClick={handleNext} className="cursor-pointer" />
       </div>
     </div>
   );
