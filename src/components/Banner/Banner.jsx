7d488d38ad61fcebd6a0a04d165211d168e13c0e
Basit Ali <opoali911@gmail.com>
2025-08-28 23:01:48 +0500

    fix the padding isssue in btn component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,22 +4,22 @@ import Button from "../Button/Button";
 
 export default function Banner() {
   return (
-    <div className="w-full sm:max-w-[1170px] h-[500px] px-4 sm:px-6 lg:px-8 flex flex-col justify-center mx-auto mt-10 mb-5  bg-amber-100">
-      <div>
-        <p>Categories</p>
+    <div className="w-full sm:max-w-[1000px] h-[400px] px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row sm:items-center gap-40 justify-center mx-auto mt-10 mb-5  bg-bgPrimary text-secondaryText">
+      <div className="flex flex-col gap-10">
+        <p className="">Categories</p>
         <h1>Enhance Your Music Experience</h1>
         <div>
             <CountDownTime/>
         </div>
         <div>
-           <Button btnLink={""} btnPx={8} btnPy={3} btnText={"Buy Now"}/>
+           <Button btnLink={""} btnPx={20} btnPy={10} btnText={"Buy Now"}/>
         </div>
       </div>
       <div className="flex justify-center items-center min-h-[300px]">
           <img
             src='/Banner/banner.png'
             alt="Banner Image"
-            className="w-52 sm:w-[500px] h-[300px] object-contain "
+            className="w-52 sm:w-[400px] h-[400px] object-contain "
           />
         </div>
     </div>
