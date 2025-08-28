92de47e2090f306f7dda73c6e389944d1a4216ee
Basit Ali <opoali911@gmail.com>
2025-08-28 23:01:33 +0500

    now start working  banner component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 
 export default function Button({btnText,btnPx, btnPy, btnLink}) {
   return (
-    <Link href={btnLink} className={`bg-bgBtn px-${btnPx} py-${btnPy} text-center rounded-sm  text-secondaryText`}>{btnText}</Link>
+    <Link
+     href={btnLink}
+     style={{ padding: `${btnPy}px ${btnPx}px` }}
+     className={`bg-bgBtn  text-center rounded-sm  text-secondaryText`}>{btnText}</Link>
   )
 }
