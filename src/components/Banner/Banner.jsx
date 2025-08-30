339774293f1b46bbafa175a8ad4e52538f78acaf
Basit Ali <opoali911@gmail.com>
2025-08-30 23:29:07 +0500

    complete the banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,25 +4,26 @@ import Button from "../Button/Button";
 
 export default function Banner() {
   return (
-    <div className="w-full sm:max-w-[1000px] h-auto sm:h-[400px] px-4 sm:px-6 lg:px-8 flex flex-col-reverse sm:flex-row sm:items-center gap-5 sm:gap-40 justify-center mx-auto mt-10 mb-5  bg-bgPrimary text-secondaryText rounded-sm">
+    <div className="w-full sm:max-w-[1100px] h-auto sm:h-[400px] px-4 sm:px-6 lg:px-8 flex flex-col-reverse sm:flex-row sm:items-center gap-5 sm:gap-40 justify-center mx-auto mt-10 mb-5  bg-bgPrimary text-secondaryText rounded-sm">
       <div className="flex justify-center items-center sm:justify-normal sm:items-start flex-col gap-5 mb-10 sm:mb-0">
         <p className="text-accent">Categories</p>
-        <h1 className="text-xl sm:text-xl lg:text-4xl">Enhance Your Music Experience</h1>
+        <h1 className="text-xl sm:text-xl lg:text-5xl">
+          Enhance Your Music Experience
+        </h1>
         <div>
-            <CountDownTime/>
+          <CountDownTime />
         </div>
-       <div className="sm:mt-7 w-full sm:w-auto flex justify-center sm:justify-start">
-  <Button btnLink="" btnPx={30} btnPy={10} btnText="Buy Now" />
-</div>
-
+        <div className="sm:mt-7 w-full sm:w-auto flex justify-center sm:justify-start">
+          <Button btnLink="" btnPx={30} btnPy={10} btnText="Buy Now" />
+        </div>
       </div>
       <div className="flex justify-center items-center min-h-[300px]">
-          <img
-            src='/Banner/banner.png'
-            alt="Banner Image"
-            className="w-[300px] sm:w-[400px] sm:h-[400px] object-contain "
-          />
-        </div>
+        <img
+          src="/Banner/banner.png"
+          alt="Banner Image"
+          className="w-[300px] sm:w-[600px]  object-contain "
+        />
+      </div>
     </div>
   );
 }
