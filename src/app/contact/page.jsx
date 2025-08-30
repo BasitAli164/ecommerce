0f62491946c4d695b987f2d3a4df65e1eb986beb
Basit Ali <opoali911@gmail.com>
2025-08-30 23:29:58 +0500

    working in cart component

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
   <>
     {
       cartData.map((item)=>(
-        <Cart key={item.id} pordDetail={item}/>
+        <Cart key={item.id} prodDetail={item}/>
       ))
     }
   </>
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,12 @@
 
-export default function Cart({pordDetail}) {
-    console.log("Cart data from Cart component:",pordDetail)
+export default function Cart({prodDetail}) {
+    console.log("Cart data from Cart component:",prodDetail)
     
   return (
     <div>
-     <div></div>
+     <div style={{backgroundImage:`${prodDetail.image}` ,backgroundPosition:"center", backgroundRepeat:"no-repeat",backgroundSize:"cover"}} className="w-10 h-10">
+      
+     </div>
      <div></div>
     </div>
   )
