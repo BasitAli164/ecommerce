e2088b2e02ca4bc412eaaea3c32357def4f5ae23
Basit Ali <opoali911@gmail.com>
2025-08-30 23:37:31 +0500

    working in cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,8 +3,8 @@ export default function Cart({prodDetail}) {
     console.log("Cart data from Cart component:",prodDetail)
     
   return (
-    <div className="border-2 border-s-lime-500 border-solid ">
-     <div style={{backgroundImage:`url(${prodDetail.image})` ,backgroundPosition:"center", backgroundRepeat:"no-repeat",backgroundSize:"cover"}} className="w-32 h-60">
+    <div className="w-[270px] h-[350px] mt-10 border-2 border-s-lime-500 border-solid ">
+     <div style={{backgroundImage:`url(${prodDetail.image})` ,backgroundPosition:"center", backgroundRepeat:"no-repeat",backgroundSize:"cover"}} className="w-[270px]  h-[250px] border-b-2 border-amber-800">
       
      </div>
      <div></div>
