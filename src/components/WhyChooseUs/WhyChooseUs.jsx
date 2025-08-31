43fb9cd2c7b761cf70f837e53ff154802b27c128
Basit Ali <opoali911@gmail.com>
2025-08-31 19:10:06 +0500

    working new component

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -1,10 +1,36 @@
+import {   HeadphonesIcon, TruckIcon } from 'lucide-react'
 import React from 'react'
 
 export default function WhyChooseUs() {
   return (
     <div>
         <div>
-            
+            <div>
+            <TruckIcon/>
+            </div>
+            <h2>FREE AND FAST DELIVERY</h2>
+            <p>Free delivery for all orders over $140</p>
+        </div>
+        <div>
+            <div>
+            <HeadphonesIcon/>
+            </div>
+            <h2>FREE AND FAST DELIVERY</h2>
+            <p>Free delivery for all orders over $140</p>
+        </div>
+        <div>
+            <div>
+            <TruckIcon/>
+            </div>
+            <h2>24/7 CUSTOMER SERVICE</h2>
+            <p>Friendly 24/7 customer support</p>
+        </div>
+        <div>
+            <div>
+            <TruckIcon/>
+            </div>
+            <h2>MONEY BACK GUARANTEE</h2>
+            <p>We reurn money within 30 days</p>
         </div>
       
     </div>
