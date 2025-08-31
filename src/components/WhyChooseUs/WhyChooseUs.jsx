d4cc0fe6090f7023a46beaecc82932715e30ba1b
Basit Ali <opoali911@gmail.com>
2025-08-31 19:31:13 +0500

    almost compolete the whychooseus component

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -5,26 +5,26 @@ export default function WhyChooseUs() {
   return (
     <div className='w-full flex flex-col sm:flex-row justify-center items-center gap-10 sm:gap-32 my-20'  >
         <div className='flex flex-col justify-center items-center gap-1'>
-            <div>
-            <TruckIcon/>
+            <div className='bg-bgPrimary p-5 text-center text-secondaryText rounded-full'>
+            <TruckIcon className='size-10'/>
             </div>
-            <h2>FREE AND FAST DELIVERY</h2>
-            <p>Free delivery for all orders over $140</p>
+            <h2 className='text-sm sm:text-lg font-semibold text-center'>FREE AND FAST DELIVERY</h2>
+            <p className='text-center'>Free delivery for all orders over $140</p>
         </div>
         
         <div className='flex flex-col justify-center items-center gap-1'>
-            <div>
-            <HeadphonesIcon/>
+            <div className='bg-bgPrimary p-5 text-center text-secondaryText rounded-full'>
+            <HeadphonesIcon className='size-10'/>
             </div>
-            <h2>24/7 CUSTOMER SERVICE</h2>
-            <p>Friendly 24/7 customer support</p>
+            <h2 className='text-sm sm:text-lg font-semibold text-center'>24/7 CUSTOMER SERVICE</h2>
+            <p className='text-center'>Friendly 24/7 customer support</p>
         </div>
         <div className='flex flex-col justify-center items-center gap-1'>
-            <div>
-            <ShieldCheck/>
+            <div className='bg-bgPrimary p-5 text-center text-secondaryText rounded-full'>
+            <ShieldCheck className='size-10'/>
             </div>
-            <h2>MONEY BACK GUARANTEE</h2>
-            <p>We reurn money within 30 days</p>
+            <h2 className='text-sm sm:text-lg font-semibold text-center'>MONEY BACK GUARANTEE</h2>
+            <p className='text-center'>We reurn money within 30 days</p>
         </div>
       
     </div>
