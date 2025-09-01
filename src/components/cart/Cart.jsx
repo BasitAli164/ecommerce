d3b18f0f3ecbccea523eaa6dd1e1d29703ae0f46
Basit Ali <opoali911@gmail.com>
2025-09-01 18:40:18 +0500

    working in cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -7,14 +7,12 @@ export default function Cart({ prodDetail }) {
     <div className="w-[270px] h-[350px] mt-2 border-2 border-s-lime-500 border-solid ">
       <div className="w-[270px]  h-[250px] border-b-2 border-amber-800 bg-[#F5F5F5] flex  flex-col justify-between items-center">
         <div className="w-full flex justify-between ">
-          {prodDetail.discount ? (
+          {prodDetail.discount && (
             <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center mt-3 ml-3 rounded-sm">
               {prodDetail.discount}
             </div>
-          ) : (
-            <div></div>
           )}
-          <div className="flex flex-col justify-center items-center gap-1 mr-3 mt-3">
+          <div className="flex flex-col justify-center items-center gap-1 mr-3 mt-3 ml-auto">
             <div className="bg-[#fff] rounded-full flex justify-center items-center w-[34px] h-[34px]">
               <Heart className="size-5" />
             </div>
