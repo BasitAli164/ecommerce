e41c4a4635ab8f4a539ed69d591821340a191960
Basit Ali <opoali911@gmail.com>
2025-09-02 00:53:26 +0500

    category compoennt is complete

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,22 +1,21 @@
 import React, { useRef, useState } from "react";
-import { ChevronLeft,ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { categoryData } from "@/data/dummyData";
 
 export default function Category() {
-    const scrollRef=useRef(null);
-    const [acticeId,setActiveId]=useState(categoryData[5].id)
+  const scrollRef = useRef(null);
+  const [acticeId, setActiveId] = useState(categoryData[5].id);
 
-
-    const scrollLeft=()=>{
-        if(scrollRef.current){
-            scrollRef.current.scrollBy({left:-300,behavior:"smooth"})
-        }
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
     }
-    const scrollRight=()=>{
-        if(scrollRef.current){
-            scrollRef.current.scrollBy({left:300,behavior:"smooth"})
-        }
+  };
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
     }
+  };
   return (
     <div className="w-full sm:max-w-[1170px] px-4 sm:px-6 lg:px-8 flex flex-col justify-center mx-auto mt-10 mb-5 border-b">
       <div className="flex items-center gap-2">
@@ -29,25 +28,31 @@ export default function Category() {
             Browse By Category
           </h2>
           <div className="hidden sm:flex justify-around sm:justify-center items-center gap-4 sm:gap-6">
-          <ChevronLeft onClick={scrollLeft} className="cursor-pointer" />
-          <ChevronRight onClick={scrollRight} className="cursor-pointer" />
-        </div>
+            <ChevronLeft onClick={scrollLeft} className="cursor-pointer" />
+            <ChevronRight onClick={scrollRight} className="cursor-pointer" />
+          </div>
         </div>
       </div>
-        <div
+      <div
         ref={scrollRef}
-         className="flex justify-center items-center gap-5 p-5 mb-10 overflow-x-auto scroll-smooth no-scrollbar">
-            {
-            categoryData.map((item)=>(
-                <div
-                 key={item.id} 
-                 onMouseEnter={()=>setActiveId(item.id)}
-                 className={`w-40 h-40 flex-shrink-0  flex justify-center items-center border-[1px] p-5  cursor-pointer transition-all duration-700 ease-in-out delay-100 ${acticeId===item.id ?"bg-accent text-secondaryText":"hover:bg-accent hover:text-secondaryText"}`}>
-                    <h3 className="text-sm sm:text-lg lg:text-xl">{item.categoryName}</h3>                    
-                </div>
-            ))
-        }
-        </div>
+        className="flex justify-center items-center gap-5 p-5 mb-10 overflow-x-auto scroll-smooth no-scrollbar"
+      >
+        {categoryData.map((item) => (
+          <div
+            key={item.id}
+            onMouseEnter={() => setActiveId(item.id)}
+            className={`w-40 h-40 flex-shrink-0  flex justify-center items-center border-[1px] p-5  cursor-pointer transition-all duration-700 ease-in-out delay-100 ${
+              acticeId === item.id
+                ? "bg-accent text-secondaryText"
+                : "hover:bg-accent hover:text-secondaryText"
+            }`}
+          >
+            <h3 className="text-sm sm:text-lg lg:text-xl">
+              {item.categoryName}
+            </h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
