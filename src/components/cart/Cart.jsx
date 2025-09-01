56d31513275da1fa0a245c67fc6f6d1c23b97c3d
Basit Ali <opoali911@gmail.com>
2025-09-01 18:13:14 +0500

    working in cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,12 +5,12 @@ export default function Cart({prodDetail}) {
     
   return (
     <div className="w-[270px] h-[350px] mt-2 border-2 border-s-lime-500 border-solid ">
-     <div style={{backgroundImage:`url(${prodDetail.image})` ,backgroundPosition:"center", backgroundRepeat:"no-repeat",backgroundSize:"cover"}} className="w-[270px]  h-[250px] border-b-2 border-amber-800 bg-[#F5F5F5] flex justify-between">
-      <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center">
+     <div  className="w-[270px]  h-[250px] border-b-2 border-amber-800 bg-[#F5F5F5] flex justify-between">
+      <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center mt-1 ml-1">
         {prodDetail.discount}
 
       </div>
-      <div>
+      <div className="mt-1 mr-1">
         <div>
           <Heart/>
         </div>
