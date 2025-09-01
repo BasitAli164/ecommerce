6a0ae74389f2ca9158d95699856a78822460579c
Basit Ali <opoali911@gmail.com>
2025-09-01 18:36:48 +0500

    working in cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,35 +1,39 @@
-import { Eye, Heart } from "lucide-react"
+import { Eye, Heart } from "lucide-react";
 
-export default function Cart({prodDetail}) {
-    console.log("Cart data from Cart component:",prodDetail)
-    
+export default function Cart({ prodDetail }) {
+  console.log("Cart data from Cart component:", prodDetail);
+
   return (
     <div className="w-[270px] h-[350px] mt-2 border-2 border-s-lime-500 border-solid ">
-     <div  className="w-[270px]  h-[250px] border-b-2 border-amber-800 bg-[#F5F5F5] flex  flex-col justify-between items-center">
-    <div className="w-full flex justify-between ">
-            {
-              prodDetail.discount ?(
-                <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center mt-3 ml-3 rounded-sm">
-        {prodDetail.discount}
-
-      </div>
-              ):(
-                <div></div>
-              )
-            }
-      <div className="flex flex-col justify-center items-center gap-1 mr-3 mt-3">
-        <div className="bg-[#fff] rounded-full flex justify-center items-center w-[34px] h-[34px]">
-          <Heart className="size-5"/>
+      <div className="w-[270px]  h-[250px] border-b-2 border-amber-800 bg-[#F5F5F5] flex  flex-col justify-between items-center">
+        <div className="w-full flex justify-between ">
+          {prodDetail.discount ? (
+            <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center mt-3 ml-3 rounded-sm">
+              {prodDetail.discount}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <div className="flex flex-col justify-center items-center gap-1 mr-3 mt-3">
+            <div className="bg-[#fff] rounded-full flex justify-center items-center w-[34px] h-[34px]">
+              <Heart className="size-5" />
+            </div>
+            <div className="bg-[#fff] rounded-full flex justify-center items-center w-[34px] h-[34px]">
+              <Eye className="size-5" />
+            </div>
+          </div>
         </div>
-        <div className="bg-[#fff] rounded-full flex justify-center items-center w-[34px] h-[34px]">
-          <Eye className="size-5"/>
-        </div>
+        <div
+          style={{
+            backgroundImage: `url(${prodDetail.image})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+          className="w-[200px] h-[200px]"
+        ></div>
       </div>
+      <div></div>
     </div>
-    <div style={{backgroundImage:`url(${prodDetail.image})`, backgroundPosition:"center", backgroundRepeat:"no-repeat" ,backgroundSize:"cover"}} className="w-[200px] h-[200px]"></div>
-      
-     </div>
-     <div></div>
-    </div>
-  )
+  );
 }
