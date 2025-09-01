749ea945f309125abb532ea4d1ecc3b276991a37
Basit Ali <opoali911@gmail.com>
2025-09-01 19:18:20 +0500

    working in cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -33,7 +33,7 @@ export default function Cart({ prodDetail }) {
         ></div>
         {
           prodDetail.inStock ?(
-            <button className="w-full bg-[#000] text-center text-[#f5f5f5] p-2 " >Add To Cart</button>
+            <button className="w-full bg-[#000] text-center text-[#f5f5f5] p-2 text-lg font-semibold " >Add To Cart</button>
           ):(
             <></>
           )
