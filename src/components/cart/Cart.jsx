d02e4eae7e023faee6acb7b54a11b05729554392
Basit Ali <opoali911@gmail.com>
2025-09-01 18:15:04 +0500

    working in cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,7 +6,8 @@ export default function Cart({prodDetail}) {
   return (
     <div className="w-[270px] h-[350px] mt-2 border-2 border-s-lime-500 border-solid ">
      <div  className="w-[270px]  h-[250px] border-b-2 border-amber-800 bg-[#F5F5F5] flex justify-between">
-      <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center mt-1 ml-1">
+     <div className="flex justify-between">
+       <div className="bg-[#DB4444] w-[55px] h-[26px] flex justify-center items-center mt-1 ml-1">
         {prodDetail.discount}
 
       </div>
@@ -18,6 +19,8 @@ export default function Cart({prodDetail}) {
           <Eye/>
         </div>
       </div>
+     </div>
+     <div></div>
       
      </div>
      <div></div>
