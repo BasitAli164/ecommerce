791118944eca5d44a51ec1567cca100c7fb2882c
Basit Ali <opoali911@gmail.com>
2025-09-02 23:18:26 +0500

    working in category component

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -15,7 +15,7 @@ export default function Category() {
         }
     }
   return (
-    <div>
+    <div className="w-full sm:max-w-[1170px] px-4 sm:px-6 lg:px-8 flex flex-col justify-center mx-auto mt-10 mb-5 border-b">
       <div className="flex items-center gap-2">
         <div className="w-3 h-8 bg-bgBtn "></div>
         <p className="text-accent font-bold text-lg sm:text-2xl">Today's</p>
@@ -23,7 +23,7 @@ export default function Category() {
       <div>
         <div className="flex flex-col sm:flex-row sm:gap-16 lg:gap-32 sm:mt-10">
           <h2 className="text-2xl sm:text-3xl lg:text-[36px] font-semibold">
-            Flash Sales
+            Category
           </h2>
           <div className="hidden sm:flex justify-around sm:justify-center items-center gap-4 sm:gap-6">
           <ChevronLeft onClick={scrollLeft} />
