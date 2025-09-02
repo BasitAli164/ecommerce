7f6baf1d84c15fb5bc815d98111feaabf0eef288
Basit Ali <opoali911@gmail.com>
2025-09-02 23:24:11 +0500

    working in categroy component

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -18,12 +18,12 @@ export default function Category() {
     <div className="w-full sm:max-w-[1170px] px-4 sm:px-6 lg:px-8 flex flex-col justify-center mx-auto mt-10 mb-5 border-b">
       <div className="flex items-center gap-2">
         <div className="w-3 h-8 bg-bgBtn "></div>
-        <p className="text-accent font-bold text-lg sm:text-2xl">Today's</p>
+        <p className="text-accent font-bold text-lg sm:text-2xl">Categories</p>
       </div>
       <div>
         <div className="flex  justify-start sm:justify-between items-center sm:gap-16 lg:gap-32 sm:mt-10">
           <h2 className="text-2xl sm:text-3xl lg:text-[36px] font-semibold">
-            Category
+            Browse By Category
           </h2>
           <div className="hidden sm:flex justify-around sm:justify-center items-center gap-4 sm:gap-6">
           <ChevronLeft onClick={scrollLeft} />
