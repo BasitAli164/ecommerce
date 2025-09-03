320450530283ba01e32dedf2f94d5bf788b78582
Basit Ali <opoali911@gmail.com>
2025-09-03 11:08:06 +0500

    working in code overview

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -33,7 +33,7 @@ export default function Cart({ prodDetail }) {
         ></div>
         {
           prodDetail.inStock ?(
-            <Link href={""} className="w-full bg-[#000] text-center text-[#f5f5f5] p-1 text-lg cursor-pointer" >Add To Cart</Link>
+            <Link href={""} className="w-full bg-[#000] text-center text-[#f5f5f5] p-1 text-lg " >Add To Cart</Link>
           ):(
             <></>
           )
