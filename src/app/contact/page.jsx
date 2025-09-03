3ac54088fc4756f31fea728c148cb3dc7c9a5cd0
Basit Ali <opoali911@gmail.com>
2025-09-03 11:13:23 +0500

    clean the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,18 +1,12 @@
 'use client'
 import Cart from '@/components/cart/Cart'
-import { useCart } from '@/context/cartContext'
 import React from 'react'
 
 
 export default function Contact() {
-  const {cartData}=useCart()
   return (
   <div className=' flex flex-wrap justify-center items-center gap-5 overflow-hidden '>
-    {
-      cartData.map((item)=>(
-        <Cart key={item.id} prodDetail={item}/>
-      ))
-    }
+   Contact page
   </div>
    
       
