601951d47a1faca53ba1cea8d556fa133a76ed8a
Basit Ali <opoali911@gmail.com>
2025-09-05 00:32:36 +0500

    work in signup page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -12,65 +12,65 @@ export default function SignUp() {
     password: "",
   });
   return (
-    <div className="w-full h-screen grid grid-cols-1 sm:grid-cols-2  justify-center items-center p-5 gap-20">
+    <div className="w-screen h-screen grid grid-cols-1 sm:grid-cols-2  justify-center items-center  gap-20">
       <div>
-        <img src="/signUp/signUp.jpg" alt="Sign up"  />
+        <img src="/signUp/signUp.jpg" alt="Sign up" />
       </div>
-      <div>
+      <div className="w-[500px]">
         <div>
-        <h1>Create an account</h1>
-        <p>Enter your details below</p>
-      </div>
-      <div>
-        <InputField
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Name"
-          value={userData.fullName}
-          onChange={(e) => setUserData(e.target.value)}
-        />
-        <InputField
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Email"
-          value={userData.email}
-          onChange={(e) => setUserData(e.target.value)}
-        />
-        <InputField
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={userData.password}
-          onChange={(e) => setUserData(e.target.value)}
-        />
-      </div>
-      <div className="inline-flex flex-col  gap-4 mt-5">
-        <Button
-          btnLink={""}
-          btnPx={80}
-          btnPy={15}
-          btnText="Create Account"
-          bgColor="#DB4444"
-          textColor="#fafafa"
-        />
-
-        <Button
-          btnLink={""}
-          btnPx={80}
-          btnPy={15}
-          btnText="Sign up with Google"
-          bgColor="transparent"
-          btnBorder={1}
-          icon={FcGoogle}
-        />
-
-        <div>
-          <p>Already have account?</p> <Link href={"/signin"}>Log in</Link>
+          <h1>Create an account</h1>
+          <p>Enter your details below</p>
         </div>
-      </div>
+        <div>
+          <InputField
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={userData.fullName}
+            onChange={(e) => setUserData(e.target.value)}
+          />
+          <InputField
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={userData.email}
+            onChange={(e) => setUserData(e.target.value)}
+          />
+          <InputField
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={userData.password}
+            onChange={(e) => setUserData(e.target.value)}
+          />
+        </div>
+        <div className="inline-flex flex-col  gap-4 mt-5">
+          <Button
+            btnLink={""}
+            btnPx={60}
+            btnPy={12}
+            btnText="Create Account"
+            bgColor="#DB4444"
+            textColor="#fafafa"
+          />
+
+          <Button
+            btnLink={""}
+            btnPx={60}
+            btnPy={12}
+            btnText="Sign up with Google"
+            bgColor="transparent"
+            btnBorder={1}
+            icon={FcGoogle}
+          />
+
+          <div>
+            <p>Already have account?</p> <Link href={"/signin"}>Log in</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
