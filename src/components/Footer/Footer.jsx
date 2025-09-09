a83d57c4049e6b018619de6e02a1cf2a43dd1fb4
Basit Ali <opoali911@gmail.com>
2025-09-09 19:02:14 +0500

    working in footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,31 @@
-'use client'
-import { Mail } from 'lucide-react';
-import React, { useState } from 'react'
+"use client";
+import { Mail } from "lucide-react";
+import React, { useState } from "react";
 
 export default function Footer() {
-  const [email,setEmail]=useState("");
+  const [email, setEmail] = useState("");
 
   return (
-    <div className='w-full h-[300px] '>
+    <div className="w-full h-[300px] ">
       <div>
         <h1>Exclusive</h1>
         <p>Subscribe</p>
         <p>Get 10% off your first order</p>
         <div>
-          <input type="text" name="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-          
-          
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
       </div>
       <div></div>
       <div></div>
       <div></div>
       <div></div>
-
-      
     </div>
-  )
+  );
 }
