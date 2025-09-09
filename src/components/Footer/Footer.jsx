7578b068ca916e1f9c40146b8b06df54a4101bcf
Basit Ali <opoali911@gmail.com>
2025-09-09 19:08:36 +0500

    working in footer component structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 "use client";
-import { Mail } from "lucide-react";
+import Link from "next/link";
 import React, { useState } from "react";
+
+
+const links=[
+  {id:1,link:'',title:"My Account"},
+  {id:2,link:'',title:"Cart"},
+  {id:3,link:'',title:"Wishlist"},
+  {id:4,link:'',title:"Shop"}
+]
+
 
 export default function Footer() {
   const [email, setEmail] = useState("");
+
 
   return (
     <div className="w-full h-[300px] ">
@@ -22,8 +32,21 @@ export default function Footer() {
           />
         </div>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <h2>Support</h2>
+        <p>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
+        <p>exclusive@gmail.com</p>
+        <p>+88015-88888-9999</p>
+      </div>
+      <div>
+        <h2>Account</h2>
+        {
+          links.map((item)=>(
+            <Link key={item.id} href={item.link}>{item.title}</Link>
+          ))
+        }
+        
+      </div>
       <div></div>
       <div></div>
     </div>
