054f313209cba56a217fe8e15f30749e0735e800
Basit Ali <opoali911@gmail.com>
2025-09-09 18:49:55 +0500

    working in footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Footer() {
+  const [email,setEmail]=useState("");
+
   return (
     <div className='w-full h-[300px] bg-bgPrimary'>
-      
+      <div>
+        <h1>Exclusive</h1>
+        <p>Subscribe</p>
+        <p>Get 10% off your first order</p>
+        <div>
+          <input type="text" name="" id="" value={email} onChange={(e)=>setEmail()}/>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
       
     </div>
   )
