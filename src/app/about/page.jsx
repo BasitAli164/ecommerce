78b1a4341b1510f8445f3f7a4edf00e26b46aae6
Basit Ali <opoali911@gmail.com>
2025-10-15 21:08:57 +0500

    start again to make the ecommerce website

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+     <div>
+      <div>
+        <h1></h1>
+      </div>
+      <div></div>
+     </div>
     </div>
   )
 }
