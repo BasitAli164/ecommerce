637d0ae110f971e2a649a53e08baeed6bbac6f09
Basit Ali <opoali911@gmail.com>
2025-12-13 21:35:20 +0500

    create a not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>
-        <h2 className='text-9xl text-black'>404 Not Found</h2>
-        <p className='text-lg'>Your visited page not found. You may go home page.</p>
+    <div className='h-screen w-full flex justify-center items-center flex-col gap-4 lg:gap-6'>
+        <h2 className='text-4xl lg:text-6xl xl:text-8xl text-black'>404 Not Found</h2>
+        <p className='text-[10px] lg:text-lg text-center'>Your visited page not found. You may go home page.</p>
         <Button bgColor={"#DB4444"} btnLink={"/"} textColor={"#fff"} btnText={"Back to Home Page"} btnPx={24} btnPy={8}/>
 
 
