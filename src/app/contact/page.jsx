a149db39388ca7f161a5e2a12a3c61a5880525ad
Basit Ali <opoali911@gmail.com>
2025-12-14 00:04:28 +0500

    working in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,29 +1,30 @@
 'use client'
+import Button from '@/components/Button/Button'
 import {  Mail, PhoneCallIcon } from 'lucide-react'
 import React from 'react'
 
 
 export default function Contact() {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div><PhoneCallIcon/></div>
-            <h2>Call To Us</h2>
+    <div className='h-screen w-full flex flex-col lg:flex-row justify-center items-center gap-32 '>
+      <div className='flex flex-col gap-10 shadow-2xl h-[450px] w-1/3 justify-center rounded-lg'>
+        <div className='ml-10'>
+          <div className='flex gap-2 items-center'>
+            <div className='rounded-full p-3 bg-accent'><PhoneCallIcon size={20} color='#fff'/></div>
+            <h2 className='font-semibold'>Call To Us</h2>
           </div>
-          <div>
-            <p>We are available 24/7, 7 days a week.</p>
-            <p>Phone: +8801611112222</p>
+          <div className='flex flex-col gap-1 mt-2'>
+            <p className='text-justify'>We are available 24/7, 7 days a week.</p>
+            <p className='text-justify'>Phone: +8801611112222</p>
           </div>
 
         </div>
-        <div>
-          <div>
-            <div><Mail/></div>
-            <h2>Write To Us</h2>
+        <div className='ml-10'>
+          <div className='flex gap-2 items-center'>
+            <div className='rounded-full p-3 bg-accent'><Mail size={20} color='#fff'/></div>
+            <h2 className='font-semibold'>Write To Us</h2>
           </div>
-          <div>
+          <div className='flex flex-col gap-1 mt-2'>
             <p>Fill out our form and we will contact you within 24 hours.</p>
             <p>Emails: customer@exclusive.com</p>
             <p>Emails: support@exclusive.com</p>
@@ -31,7 +32,18 @@ export default function Contact() {
         </div>
       </div>
       <div>
-        <form >
+        <form>
+          <div>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+          </div>
+          <div>
+            <textarea name="" id=""></textarea>
+          </div>
+          <div>
+            <button className='px-9 py-3 bg-accent text-white'>Send Message</button>
+          </div>
 
         </form>
         
